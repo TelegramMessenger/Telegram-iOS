4e39526364a1787a61a22d361d994db6691e4443
Stephan Diederich <stephan.diederich@googlemail.com>
2011-12-05 23:29:14 +0100

    add default xcodeproj
    minor adjustments for paths and such

---
+++ Support/HockeySDKTests/HockeySDKTests.h
@@ -0,0 +1,13 @@
+//
+//  HockeySDKTests.h
+//  HockeySDKTests
+//
+//  Created by Stephan Diederich on 05.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HockeySDKTests : SenTestCase
+
+@end
