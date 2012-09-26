36c81ac65079770b50441ecb959c2f8a5566d2a7
Stephan Diederich <stephan.diederich@googlemail.com>
2012-09-26 14:10:04 +0200

    cleanup JIRA_MOBILE_CONNECT_SUPPORT_ENABLED defines
    * only define JIRA_MOBILE_CONNECT_SUPPORT_ENABLED if it's not already defined.
      default is to disable it
    * cleanup usage of JIRA_MOBILE_CONNECT_SUPPORT_ENABLED
      it's always defined, so no need to check

--- Classes/HockeySDKPrivate.h
+++ Classes/HockeySDKPrivate.h
@@ -50,8 +50,11 @@
 #define BITHOCKEYSDK_BUNDLE @"HockeySDKResources.bundle"
 #define BITHOCKEYSDK_URL @"https://sdk.hockeyapp.net/"
 
-/* Whether to compile in support for Jira Mobile Connect */
-#define JIRA_MOBILE_CONNECT_SUPPORT_ENABLED 0
+// Whether to compile in support for Jira Mobile Connect
+// default is disabled
+#ifndef JIRA_MOBILE_CONNECT_SUPPORT_ENABLED
+  #define JIRA_MOBILE_CONNECT_SUPPORT_ENABLED 0
+#endif
 
 #define BITHockeyLog(fmt, ...) do { if([BITHockeyManager sharedHockeyManager].isDebugLogEnabled && ![BITHockeyManager sharedHockeyManager].isAppStoreEnvironment) { NSLog((@"[HockeySDK] %s/%d " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__); }} while(0)
 
