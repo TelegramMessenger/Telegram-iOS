003de59f3d9f6c152b5b6db95df7184a45abedf2
Andreas Linde <mail@andreaslinde.de>
2012-10-15 00:47:50 +0200

    If an email field is entered, validate the string and only allow addresses that look correct

--- Classes/BITFeedbackUserDataViewController.m
+++ Classes/BITFeedbackUserDataViewController.m
@@ -106,6 +106,20 @@
 
 #pragma mark - Private methods
 
+- (BOOL)validateEmail {
+  NSString *emailRegex =
+  @"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
+  @"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
+  @"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"
+  @"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"
+  @"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"
+  @"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
+  @"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+  NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+  
+  return [emailTest evaluateWithObject:self.email];
+}
+
 - (BOOL)allRequiredFieldsEntered {
   if ([self.manager requireUserName] == BITFeedbackUserDataElementRequired && [self.name length] == 0)
     return NO;
@@ -113,6 +127,9 @@
   if ([self.manager requireUserEmail] == BITFeedbackUserDataElementRequired && [self.email length] == 0)
     return NO;
 
+  if ([self.email length] > 0 && ![self validateEmail])
+    return NO;
+  
   return YES;
 }
 
