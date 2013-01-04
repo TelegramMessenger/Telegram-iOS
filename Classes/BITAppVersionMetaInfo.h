9f2f40bc067d5e687ff59c51b0068600d22831d8
Andreas Linde <mail@andreaslinde.de>
2013-01-04 14:44:44 +0100

    Update copyright

--- Classes/BITAppVersionMetaInfo.h
+++ Classes/BITAppVersionMetaInfo.h
@@ -1,7 +1,7 @@
 /*
  * Author: Peter Steinberger
  *
- * Copyright (c) 2012 HockeyApp, Bit Stadium GmbH.
+ * Copyright (c) 2012-2013 HockeyApp, Bit Stadium GmbH.
  * Copyright (c) 2011 Andreas Linde, Peter Steinberger.
  * All rights reserved.
  *
--- Classes/BITCrashReportTextFormatter.m
+++ Classes/BITCrashReportTextFormatter.m
@@ -4,9 +4,9 @@
  *  Damian Morris <damian@moso.com.au>
  *  Andreas Linde <mail@andreaslinde.de>
  *
- * Copyright (c) 2008-2012 Plausible Labs Cooperative, Inc.
+ * Copyright (c) 2008-2013 Plausible Labs Cooperative, Inc.
  * Copyright (c) 2010 MOSO Corporation, Pty Ltd.
- * Copyright (c) 2012 HockeyApp, Bit Stadium GmbH.
+ * Copyright (c) 2012-2013 HockeyApp, Bit Stadium GmbH.
  * All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person
