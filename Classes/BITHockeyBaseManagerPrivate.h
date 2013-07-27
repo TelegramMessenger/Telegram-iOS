d39c39feb73e3d6f12dbacea34dc96924dfae1be
Stephan Diederich <stephan.diederich@gmail.com>
2013-07-27 14:43:25 +0200

    comment private header

--- Classes/BITHockeyBaseManagerPrivate.h
+++ Classes/BITHockeyBaseManagerPrivate.h
@@ -20,20 +20,38 @@
 
 - (void)startManager;
 
-- (void)reportError:(NSError *)error;
-- (NSString *)encodedAppIdentifier;
+/** the value this object was initialized with */
 - (BOOL)isAppStoreEnvironment;
 
+/** by default, just logs the message
+ 
+ can be overriden by subclasses to do their own error handling, 
+ e.g. to show UI
+ */
+- (void)reportError:(NSError *)error;
+
+/** url encoded version of the appIdentifier
+ 
+ where appIdentifier is either the value this object was initialized with,
+ or the main bundles CFBundleIdentifier if appIdentifier ist nil
+ */
+- (NSString *)encodedAppIdentifier;
+
+/** device / application helpers */
 - (NSString *)getDevicePlatform;
 - (NSString *)executableUUID;
 
+/** UI helpers */
 - (UIWindow *)findVisibleWindow;
 - (void)showView:(UIViewController *)viewController;
 
+/** Network helpers */
 - (NSData *)appendPostValue:(NSString *)value forKey:(NSString *)key;
 
+/** Date helpers */
 - (NSDate *)parseRFC3339Date:(NSString *)dateString;
 
+/** keychain helpers */
 - (BOOL)addStringValueToKeychain:(NSString *)stringValue forKey:(NSString *)key;
 - (NSString *)stringValueFromKeychainForKey:(NSString *)key;
 - (BOOL)removeKeyFromKeychain:(NSString *)key;
