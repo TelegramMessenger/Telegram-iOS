54b3c57ee256aba31358c4a70aed865396e8cd04
Andreas Linde <mail@andreaslinde.de>
2014-02-20 16:33:31 +0100

    Remove #ifdef for clang analyzer since the false warning from 4.2b3 doesn't appear any longer
    Tested with Xcode 5.0.2

--- Classes/BITHockeyHelper.m
+++ Classes/BITHockeyHelper.m
@@ -458,10 +458,7 @@ UIImage *bit_newWithContentsOfResolutionIndependentFile(NSString * path) {
 
 
 UIImage *bit_imageWithContentsOfResolutionIndependentFile(NSString *path) {
-#ifndef __clang_analyzer__
-  // clang alayzer in 4.2b3 thinks here's a leak, which is not the case.
   return bit_newWithContentsOfResolutionIndependentFile(path);
-#endif
 }
 
 
