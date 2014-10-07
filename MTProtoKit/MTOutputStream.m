c034db4abdf6b08ffcc81847efd0c520030416f8
Peter <peter@Peters-MacBook-Pro-2.local>
2014-10-07 19:31:27 +0400

    Conversation activities

--- MTProtoKit/MTOutputStream.m
+++ MTProtoKit/MTOutputStream.m
@@ -14,18 +14,7 @@
 
 static inline int roundUp(int numToRound, int multiple)
 {
-    if (multiple == 0)
-    {
-        return numToRound;
-    }
-    
-    int remainder = numToRound % multiple;
-    if (remainder == 0)
-    {
-        return numToRound;
-    }
-    
-    return numToRound + multiple - remainder;
+    return multiple == 0 ? numToRound : ((numToRound % multiple) == 0 ? numToRound : (numToRound + multiple - (numToRound % multiple)));
 }
 
 @interface MTOutputStream ()
