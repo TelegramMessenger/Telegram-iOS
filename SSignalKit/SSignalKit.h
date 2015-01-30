2066b31b057805c6ae229c3c59a0358d48724fba
Peter <iakovlev.peter.p@gmail.com>
2015-01-31 00:18:30 +0300

    Initial Commit

---
+++ SSignalKit/SSignalKit.h
@@ -0,0 +1,19 @@
+//
+//  SSignalKit.h
+//  SSignalKit
+//
+//  Created by Peter on 31/01/15.
+//  Copyright (c) 2015 Telegram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSignalKit.
+FOUNDATION_EXPORT double SSignalKitVersionNumber;
+
+//! Project version string for SSignalKit.
+FOUNDATION_EXPORT const unsigned char SSignalKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSignalKit/PublicHeader.h>
+
+
