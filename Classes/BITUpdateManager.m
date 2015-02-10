06f73f10d35cfc2e4bf4efc48c38e1de4e229841
Andreas Linde <anlinde@microsoft.com>
2015-02-10 14:47:37 +0100

    Fix Xcode 6.3 Beta compiler warnings

--- Classes/BITUpdateManager.m
+++ Classes/BITUpdateManager.m
@@ -606,7 +606,7 @@ typedef NS_ENUM(NSInteger, BITUpdateAlertViewTag) {
   if (!self.checkInProgress) {
     
     if (!_lastUpdateCheckFromBlockingScreen ||
-        abs([NSDate timeIntervalSinceReferenceDate] - [_lastUpdateCheckFromBlockingScreen timeIntervalSinceReferenceDate]) > 60) {
+        fabs([NSDate timeIntervalSinceReferenceDate] - [_lastUpdateCheckFromBlockingScreen timeIntervalSinceReferenceDate]) > 60) {
       _lastUpdateCheckFromBlockingScreen = [NSDate date];
       [self checkForUpdateShowFeedback:NO];
     }
