9e1674f2232ea410771b088e195a0c751e7b80c9
Andreas Linde <anlinde@microsoft.com>
2015-06-12 15:02:17 +0200

    Add support for __nullable on older Xcode versions

--- Classes/HockeySDK.h
+++ Classes/HockeySDK.h
@@ -27,6 +27,28 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
+// Define nullability fallback for backwards compatibility
+#if !__has_feature(nullability)
+#define NS_ASSUME_NONNULL_BEGIN
+#define NS_ASSUME_NONNULL_END
+#define nullable
+#define nonnull
+#define null_unspecified
+#define null_resettable
+#define __nullable
+#define __nonnull
+#define __null_unspecified
+#endif
+
+// Fallback for convenience syntax which might not be available in older SDKs
+#ifndef NS_ASSUME_NONNULL_BEGIN
+#define NS_ASSUME_NONNULL_BEGIN _Pragma("clang assume_nonnull begin")
+#endif
+#ifndef NS_ASSUME_NONNULL_END
+#define NS_ASSUME_NONNULL_END _Pragma("clang assume_nonnull end")
+#endif
+
+
 #import "HockeySDKFeatureConfig.h"
 
 #import "BITHockeyManager.h"
