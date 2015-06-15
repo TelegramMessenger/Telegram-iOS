67bae10549f0db16e1715adfcfa7fecedaf93771
Ilya Laktyushin <ilya@laktyush.in>
2015-06-15 17:51:14 +0300

    SPipe replay

--- SSignalKit/SSignal+Pipe.h
+++ SSignalKit/SSignal+Pipe.h
@@ -5,5 +5,7 @@
 @property (nonatomic, copy, readonly) SSignal *(^signalProducer)();
 @property (nonatomic, copy, readonly) void (^sink)(id);
 
+- (instancetype)initWithReplay:(bool)replay;
+
 @end
 
--- SSignalKit/SSignal+Pipe.m
+++ SSignalKit/SSignal+Pipe.m
@@ -4,14 +4,42 @@
 #import "SAtomic.h"
 #import "SBag.h"
 
+@interface SPipeReplayState : NSObject
+
+@property (nonatomic, readonly) bool hasReceivedValue;
+@property (nonatomic, strong, readonly) id recentValue;
+
+@end
+
+@implementation SPipeReplayState
+
+- (instancetype)initWithReceivedValue:(bool)receivedValue recentValue:(id)recentValue
+{
+    self = [super init];
+    if (self != nil)
+    {
+        _hasReceivedValue = receivedValue;
+        _recentValue = recentValue;
+    }
+    return self;
+}
+
+@end
+
 @implementation SPipe
 
 - (instancetype)init
+{
+    return [self initWithReplay:false];
+}
+
+- (instancetype)initWithReplay:(bool)replay
 {
     self = [super init];
     if (self != nil)
     {
         SAtomic *subscribers = [[SAtomic alloc] initWithValue:[[SBag alloc] init]];
+        SAtomic *replayState = replay ? [[SAtomic alloc] initWithValue:[[SPipeReplayState alloc] initWithReceivedValue:false recentValue:nil]] : nil;
         
         _signalProducer = [^SSignal *
         {
@@ -26,6 +54,16 @@
                     } copy]];
                     return nil;
                 }];
+                
+                if (replay)
+                {
+                    [replayState with:^id(SPipeReplayState *state)
+                    {
+                        if (state.hasReceivedValue)
+                            [subscriber putNext:state.recentValue];
+                        return nil;
+                    }];
+                }
                 
                 return [[SBlockDisposable alloc] initWithBlock:^
                 {
@@ -44,9 +82,18 @@
             {
                 return [bag copyItems];
             }];
+            
             for (void (^item)(id) in items)
             {
                 item(next);
+            }
+            
+            if (replay)
+            {
+                [replayState modify:^id(__unused SPipeReplayState *state)
+                {
+                    return [[SPipeReplayState alloc] initWithReceivedValue:true recentValue:next];
+                }];
             }
         } copy];
     }
