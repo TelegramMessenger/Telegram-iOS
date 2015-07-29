690b1d572b0ae91b5943b8781418900338b2a2a7
Peter <iakovlev.peter.p@gmail.com>
2015-07-29 19:06:10 +0300

    Initial Commit

---
+++ Display/Display.h
@@ -0,0 +1,19 @@
+//
+//  Display.h
+//  Display
+//
+//  Created by Peter on 29/07/15.
+//  Copyright © 2015 Telegram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Display.
+FOUNDATION_EXPORT double DisplayVersionNumber;
+
+//! Project version string for Display.
+FOUNDATION_EXPORT const unsigned char DisplayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Display/PublicHeader.h>
+
+
