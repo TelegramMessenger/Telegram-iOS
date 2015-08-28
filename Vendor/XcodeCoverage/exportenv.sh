cd1d13f233d19ec1be95b6a393647755d4d104f8
Lukas Spieß <lspiess@microsoft.com>
2015-08-28 14:21:19 +0200

    Remove XcodeCoverage from test targets
    Since we have to require Xcode 7 for Bitcode support anyway, we can also switch to using Xcode's native code coverage support

--- Vendor/XcodeCoverage/exportenv.sh
+++
@@ -1,8 +0,0 @@
-#
-#   Copyright 2012 Jonathan M. Reid. See LICENSE.txt
-#   Created by: Jon Reid, http://qualitycoding.org/
-#   Source: https://github.com/jonreid/XcodeCoverage
-#
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export | egrep '( BUILT_PRODUCTS_DIR)|(CURRENT_ARCH)|(OBJECT_FILE_DIR_normal)|(SRCROOT)|(OBJROOT)' > ${DIR}/env.sh
