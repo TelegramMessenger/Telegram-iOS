3a8e878e084d457376142179d51c068201105560
Christoph Wendt <chwend@microsoft.com>
2015-08-31 19:32:37 -0700

    Add contract files for SessionState events

---
+++ Classes/BITDomain.m
@@ -0,0 +1,44 @@
+#import "BITDomain.h"
+/// Data contract class for type Domain.
+@implementation BITDomain
+@synthesize envelopeTypeName = _envelopeTypeName;
+@synthesize dataTypeName = _dataTypeName;
+
+/// Initializes a new instance of the class.
+- (instancetype)init {
+    if (self = [super init]) {
+        _envelopeTypeName = @"Microsoft.ApplicationInsights.Domain";
+        _dataTypeName = @"Domain";
+    }
+    return self;
+}
+
+///
+/// Adds all members of this class to a dictionary
+/// @param dictionary to which the members of this class will be added.
+///
+- (BITOrderedDictionary *)serializeToDictionary {
+    BITOrderedDictionary *dict = [super serializeToDictionary];
+    return dict;
+}
+
+#pragma mark - NSCoding
+
+- (instancetype)initWithCoder:(NSCoder *)coder {
+  self = [super initWithCoder:coder];
+  if(self) {
+    _envelopeTypeName = [coder decodeObjectForKey:@"_envelopeTypeName"];
+    _dataTypeName = [coder decodeObjectForKey:@"_dataTypeName"];
+  }
+
+  return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+  [super encodeWithCoder:coder];
+  [coder encodeObject:_envelopeTypeName forKey:@"_envelopeTypeName"];
+  [coder encodeObject:_dataTypeName forKey:@"_dataTypeName"];
+}
+
+
+@end
