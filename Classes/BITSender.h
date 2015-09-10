011972245d67bc8290750156a7acb8c6fc27202b
Christoph Wendt <chwend@microsoft.com>
2015-09-09 18:46:49 -0700

    Add sender

---
+++ Classes/BITSender.h
@@ -0,0 +1,115 @@
+#import <Foundation/Foundation.h>
+#import "HockeySDK.h"
+
+#if HOCKEYSDK_FEATURE_TELEMETRY
+
+#import "HockeySDKNullability.h"
+@class BITPersistence;
+
+NS_ASSUME_NONNULL_BEGIN
+/**
+ * Utility class that's responsible for sending a bundle of data to the server
+ */
+@interface BITSender : NSObject
+
+///-----------------------------------------------------------------------------
+/// @name Initialize instance
+///-----------------------------------------------------------------------------
+
+/**
+ *  Initializes a sender instance with a given persistence object.
+ *
+ *  @param persistence used for loading files before sending them out
+ *  @param serverURL the endpoint URL for telemetry data
+ *  @return an initialized sender instance
+ */
+- (instancetype)initWithPersistence:(BITPersistence *)persistence serverURL:(NSURL *)serverURL;
+
+/**
+ *	Access to the internal operation queue
+ */
+@property (nonatomic, strong) NSOperationQueue *operationQueue;
+
+/**
+ *  A queue which is used to handle BITHTTPOperation completion blocks.
+ */
+@property (nonatomic, strong) dispatch_queue_t senderQueue;
+
+/**
+ *  The endpoint url of the telemetry server.
+ */
+@property (nonatomic, copy) NSString *endpointPath;
+
+/**
+ *  The max number of request that can run at a time.
+ */
+@property NSUInteger maxRequestCount;
+
+/**
+ *  The number of requests that are currently running.
+ */
+@property NSUInteger runningRequestsCount;
+
+/**
+ *	BaseURL to which relative paths are appended.
+ */
+@property (nonatomic, strong, readonly) NSURL *serverURL;
+
+/**
+ *  The persistence instance used for loading files before sending them out.
+ */
+@property (nonatomic, strong, readonly) BITPersistence *persistence;
+
+///-----------------------------------------------------------------------------
+/// @name Sending data
+///-----------------------------------------------------------------------------
+
+/**
+ *  Triggers sending the saved data. Does nothing if nothing has been persisted, yet. This method should be called by BITTelemetryManager on app start.
+ */
+- (void)sendSavedData;
+
+/**
+ *  Creates a HTTP operation and puts it to the queue.
+ *
+ *  @param request a request for sending a data object to the telemetry server
+ *  @param path path to the file which should be sent
+ *  @param urlSessionSupported a flag which determines whether to use NSURLConnection or NSURLSession for sending out data
+ */
+- (void)sendRequest:(NSURLRequest * __nonnull)request path:(NSString * __nonnull)path urlSessionSupported:(BOOL)isUrlSessionSupported;
+
+///-----------------------------------------------------------------------------
+/// @name Helper
+///-----------------------------------------------------------------------------
+
+/**
+ *  Returns a request for sending data to the telemetry sender.
+ *
+ *  @param data the data which should be sent
+ *
+ *  @return a request which contains the given data
+ */
+- (NSURLRequest *)requestForData:(NSData *)data withContentType:(NSString *)contentType;
+
+/**
+ *  Returns if data should be deleted based on a given status code.
+ *
+ *  @param statusCode the status code which is part of the response object
+ *
+ *  @return YES if data should be deleted, NO if the payload should be sent at a later time again.
+ */
+- (BOOL)shouldDeleteDataWithStatusCode:(NSInteger)statusCode;
+
+/**
+ *  This method tries to detect whether the given data object is regular JSON or JSON Stream and returns the appropriate HTTP content type.
+ *
+ *  @param data The data object whose content type should be returned.
+ *
+ *  @returns "application/json" if the data is regular JSON or "application/x-json-stream" if it is JSON Stream. Defaults to "application/json".
+ */
+- (NSString *)contentTypeForData:(NSData *)data;
+
+@end
+NS_ASSUME_NONNULL_END
+
+#endif /* HOCKEYSDK_FEATURE_TELEMETRY */
---
+++ Classes/BITSender.m
@@ -0,0 +1,186 @@
+#import "BITSender.h"
+#import "BITPersistencePrivate.h"
+#import "BITGZIP.h"
+#import "HockeySDKPrivate.h"
+#import "BITHTTPOperation.h"
+#import "BITHockeyHelper.h"
+
+static char const *kPersistenceQueueString = "net.hockeyapp.senderQueue";
+static NSUInteger const defaultRequestLimit = 10;
+
+@implementation BITSender
+
+@synthesize runningRequestsCount = _runningRequestsCount;
+@synthesize persistence = _persistence;
+
+#pragma mark - Initialize instance
+
+- (instancetype)initWithPersistence:(BITPersistence *)persistence serverURL:(NSURL *)serverURL {
+  if ((self = [super init])) {
+    _senderQueue = dispatch_queue_create(kPersistenceQueueString, DISPATCH_QUEUE_CONCURRENT);
+    _maxRequestCount = defaultRequestLimit;
+    _serverURL = serverURL;
+    _persistence = persistence;
+    [self registerObservers];
+  }
+  return self;
+}
+
+#pragma mark - Handle persistence events
+
+- (void)registerObservers{
+  NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+  __weak typeof(self) weakSelf = self;
+  [center addObserverForName:BITPersistenceSuccessNotification
+                      object:nil
+                       queue:nil
+                  usingBlock:^(NSNotification *notification) {
+                    typeof(self) strongSelf = weakSelf;
+                    [strongSelf sendSavedData];
+                  }];
+}
+
+#pragma mark - Sending
+
+- (void)sendSavedData{
+  @synchronized(self){
+    if(_runningRequestsCount < _maxRequestCount){
+      _runningRequestsCount++;
+    }else{
+      return;
+    }
+  }
+  __weak typeof(self) weakSelf = self;
+  dispatch_async(self.senderQueue, ^{
+    typeof(self) strongSelf = weakSelf;
+    NSString *path = [self.persistence requestNextPath];
+    NSData *data = [self.persistence dataAtPath:path];
+    [strongSelf sendData:data withPath:path];
+  });
+}
+
+- (void)sendData:(NSData * __nonnull)data withPath:(NSString * __nonnull)path {
+  if(data && data.length > 0) {
+    NSString *contentType = [self contentTypeForData:data];
+
+    NSData *gzippedData = [data gzippedData];
+    NSURLRequest *request = [self requestForData:gzippedData withContentType:contentType];
+    id nsurlsessionClass = NSClassFromString(@"NSURLSessionUploadTask");
+    BOOL isUrlSessionSupported = (nsurlsessionClass && !bit_isRunningInAppExtension());
+    
+    [self sendRequest:request path:path urlSessionSupported:isUrlSessionSupported];
+  } else {
+    self.runningRequestsCount -= 1;
+  }
+}
+
+- (void)sendRequest:(NSURLRequest * __nonnull)request path:(NSString * __nonnull)path urlSessionSupported:(BOOL)isUrlSessionSupported{
+  if(!path || !request) return;
+  __weak typeof(self) weakSelf = self;
+  
+  if(!isUrlSessionSupported) {
+    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration];
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler: ^(NSData *data, NSURLResponse *response, NSError *error) {
+                                              typeof (self) strongSelf = weakSelf;
+                                              NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+                                              NSInteger statusCode = httpResponse.statusCode;
+                                              [strongSelf handleResponseWithStatusCode:statusCode responseData:data filePath:path error:error];
+                                            }];
+    [task resume];
+  }else{
+    BITHTTPOperation *operation = [BITHTTPOperation operationWithRequest:request];
+    [operation setCompletion:^(BITHTTPOperation *operation, NSData *responseData, NSError *error) {
+      typeof(self) strongSelf = weakSelf;
+      NSInteger statusCode = [operation.response statusCode];
+      [strongSelf handleResponseWithStatusCode:statusCode responseData:responseData filePath:path error:error];
+    }];
+    
+    [self.operationQueue addOperation:operation];
+  }
+}
+
+- (void)handleResponseWithStatusCode:(NSInteger)statusCode responseData:(NSData *)responseData filePath:(NSString *)filePath error:(NSError *)error{
+  self.runningRequestsCount -= 1;
+  
+  if(responseData && [self shouldDeleteDataWithStatusCode:statusCode]) {
+    //we delete data that was either sent successfully or if we have a non-recoverable error
+    BITHockeyLog(@"Sent data with status code: %ld", (long) statusCode);
+    BITHockeyLog(@"Response data:\n%@", [NSJSONSerialization JSONObjectWithData:responseData options:0 error:nil]);
+    [self.persistence deleteFileAtPath:filePath];
+    [self sendSavedData];
+  } else {
+    BITHockeyLog(@"Sending telemetry data failed");
+    BITHockeyLog(@"Error description: %@", error.localizedDescription);
+    [self.persistence giveBackRequestedPath:filePath];
+  }
+}
+
+#pragma mark - Helper
+
+- (NSURLRequest *)requestForData:(NSData * __nonnull)data withContentType:(NSString * __nonnull)contentType {
+
+  NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:self.serverURL];
+  request.HTTPMethod = @"POST";
+  
+  request.HTTPBody = data;
+  request.cachePolicy = NSURLRequestReloadIgnoringLocalCacheData;
+  
+  NSDictionary *headers = @{@"Charset": @"UTF-8",
+                            @"Content-Encoding": @"gzip",
+                            @"Content-Type": contentType,
+                            @"Accept-Encoding": @"gzip"};
+  [request setAllHTTPHeaderFields:headers];
+  
+  return request;
+}
+
+//some status codes represent recoverable error codes
+//we try sending again some point later
+- (BOOL)shouldDeleteDataWithStatusCode:(NSInteger)statusCode {
+  NSArray *recoverableStatusCodes = @[@429, @408, @500, @503, @511];
+
+  return ![recoverableStatusCodes containsObject:@(statusCode)];
+}
+
+- (NSString *)contentTypeForData:(NSData *)data {
+  NSString *contentType;
+  static const uint8_t LINEBREAK_SIGNATURE = (0x0a);
+  UInt8 lastByte = 0;
+  if (data && data.length > 0) {
+    [data getBytes:&lastByte range:NSMakeRange(data.length-1, 1)];
+  }
+  
+  if (data && (data.length > sizeof(uint8_t)) && (lastByte == LINEBREAK_SIGNATURE)) {
+    contentType = @"application/x-json-stream";
+  } else {
+    contentType = @"application/json";
+  }
+  return contentType;
+}
+
+#pragma mark - Getter/Setter
+
+- (NSOperationQueue *)operationQueue {
+  if(nil == _operationQueue) {
+    _operationQueue = [[NSOperationQueue alloc] init];
+    _operationQueue.maxConcurrentOperationCount = defaultRequestLimit;
+  }
+  return _operationQueue;
+}
+
+- (NSUInteger)runningRequestsCount {
+  @synchronized(self) {
+    return _runningRequestsCount;
+  }
+}
+
+- (void)setRunningRequestsCount:(NSUInteger)runningRequestsCount {
+  @synchronized(self) {
+    _runningRequestsCount = runningRequestsCount;
+  }
+}
+
+@end
