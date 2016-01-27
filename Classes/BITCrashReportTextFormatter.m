6d4197aae484ddadc3f9dc6da4aae3b58ed4b7f9
Lukas Spieß <lspiess@microsoft.com>
2016-01-27 13:52:46 +0100

    Fix userPath anonymization and add test

--- Classes/BITCrashReportTextFormatter.m
+++ Classes/BITCrashReportTextFormatter.m
@@ -32,6 +32,7 @@
  */
 
 #import "HockeySDK.h"
+#import "HockeySDKPrivate.h"
 
 #if HOCKEYSDK_FEATURE_CRASH_REPORTER
 
@@ -349,10 +350,7 @@ static const char *findSEL (const char *imageName, NSString *imageUUID, uint64_t
         
         /* Remove username from the path */
 #if TARGET_OS_SIMULATOR
-        if ([processPath length] > 0)
-          processPath = [processPath stringByAbbreviatingWithTildeInPath];
-        if ([processPath length] > 0 && [[processPath substringToIndex:1] isEqualToString:@"~"])
-          processPath = [NSString stringWithFormat:@"/Users/USER%@", [processPath substringFromIndex:1]];
+        processPath = [self anonymizedProcessPathFromProcessPath:processPath];
 #endif
       }
       
@@ -872,6 +870,30 @@ static const char *findSEL (const char *imageName, NSString *imageUUID, uint64_t
           (const uint16_t *)[imageName cStringUsingEncoding: NSUTF16StringEncoding],
           lp64 ? 16 : 8, frameInfo.instructionPointer,
           symbolString];
+}
+
+/**
+ *  Remove the user's name from a crash's process path.
+ *  This is only necessary when sending crashes from the simulator as the path
+ *  then contains the username of the Mac the simulator is running on.
+ *
+ *  @param processPath A string containing the username
+ *
+ *  @return An anonymized string where the real username is replaced by "USER"
+ */
++ (NSString *)anonymizedProcessPathFromProcessPath:(NSString *)processPath {
+  
+  NSString *anonymizedProcessPath = [NSString string];
+  
+  if (([processPath length] > 0) && [processPath hasPrefix:@"/Users/"]) {
+    NSError *error = nil;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(/Users/[^/]+/)" options:0 error:&error];
+    anonymizedProcessPath = [regex stringByReplacingMatchesInString:processPath options:0 range:NSMakeRange(0, [processPath length]) withTemplate:@"/Users/USER/"];
+    if (error) {
+      BITHockeyLog("ERROR: String replacing failed - %@", error.localizedDescription);
+    }
+  }
+  return anonymizedProcessPath;
 }
 
 @end
