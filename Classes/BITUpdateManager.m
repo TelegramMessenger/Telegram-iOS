24bc66b85be0cd62aca6ab2609c75f1cc4ae9999
Lukas Spieß <lspiess@microsoft.com>
2016-02-08 00:21:03 +0100

    Refactor UpdateManager request creation

--- Classes/BITUpdateManager.m
+++ Classes/BITUpdateManager.m
@@ -881,34 +881,37 @@ typedef NS_ENUM(NSInteger, BITUpdateAlertViewTag) {
 }
 
 - (NSURLRequest *)requestForUpdateCheck {
-  NSMutableString *parameter = [NSMutableString stringWithFormat:@"api/2/apps/%@?format=json&extended=true&sdk=%@&sdk_version=%@&uuid=%@",
-                                bit_URLEncodedString([self encodedAppIdentifier]),
-                                BITHOCKEY_NAME,
-                                BITHOCKEY_VERSION,
-                                _uuid];
+  NSString *path = [NSString stringWithFormat:@"api/2/apps/%@", self.appIdentifier];
+  NSString *urlEncodedPath = [path stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLPathAllowedCharacterSet]];
+  
+  NSMutableString *parameters = [NSMutableString stringWithFormat:@"?format=json&extended=true&sdk=%@&sdk_version=%@&uuid=%@",
+                                 BITHOCKEY_NAME,
+                                 BITHOCKEY_VERSION,
+                                 _uuid];
   
   // add installationIdentificationType and installationIdentifier if available
   if (self.installationIdentification && self.installationIdentificationType) {
-    [parameter appendFormat:@"&%@=%@",
-     bit_URLEncodedString(self.installationIdentificationType),
-     bit_URLEncodedString(self.installationIdentification)
+    [parameters appendFormat:@"&%@=%@",
+     self.installationIdentificationType,
+     self.installationIdentification
      ];
   }
   
   // add additional statistics if user didn't disable flag
-  if (self.sendUsageData) {
-    [parameter appendFormat:@"&app_version=%@&os=iOS&os_version=%@&device=%@&lang=%@&first_start_at=%@&usage_time=%@",
-     bit_URLEncodedString([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"]),
-     bit_URLEncodedString([[UIDevice currentDevice] systemVersion]),
-     bit_URLEncodedString([self getDevicePlatform]),
-     bit_URLEncodedString([[NSBundle mainBundle] preferredLocalizations][0]),
-     bit_URLEncodedString([self installationDateString]),
-     bit_URLEncodedString([self currentUsageString])
+  if (_sendUsageData) {
+    [parameters appendFormat:@"&app_version=%@&os=iOS&os_version=%@&device=%@&lang=%@&first_start_at=%@&usage_time=%@",
+     [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"],
+     [[UIDevice currentDevice] systemVersion],
+     [self getDevicePlatform],
+     [[[NSBundle mainBundle] preferredLocalizations] objectAtIndex:0],
+     [self installationDateString],
+     [self currentUsageString]
      ];
   }
+  NSString *urlEncodedParameters = [parameters stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
   
   // build request & send
-  NSString *url = [NSString stringWithFormat:@"%@%@", self.serverURL, parameter];
+  NSString *url = [NSString stringWithFormat:@"%@%@%@", self.serverURL, urlEncodedPath, urlEncodedParameters];
   BITHockeyLog(@"INFO: Sending api request to %@", url);
   
   NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]
