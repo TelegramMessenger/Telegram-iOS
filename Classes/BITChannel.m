9174ae75190f2189812eb39c52a9f5a2aea069ce
chrwend <chwend@microsoft.com>
2016-03-23 11:35:56 -0700

    Fix bug: check if queue is busy

--- Classes/BITChannel.m
+++ Classes/BITChannel.m
@@ -59,7 +59,7 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark - Queue management
 
 - (BOOL)isQueueBusy {
-  return [self.persistence isFreeSpaceAvailable];
+  return ![self.persistence isFreeSpaceAvailable];
 }
 
 - (void)persistDataItemQueue {
