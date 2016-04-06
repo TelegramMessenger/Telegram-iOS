ace088d39b769da36cdbe0265aef461f3ec13959
Lukas Spieß <lspiess@microsoft.com>
2016-04-06 22:16:23 +0200

    Properly dereference function pointer to silence warning
    Thanks @ksuther!

--- Classes/BITCrashManager.m
+++ Classes/BITCrashManager.m
@@ -115,7 +115,7 @@ static PLCrashReporterCallbacks plCrashCallbacks = {
 
 - (instancetype)initWithCXXExceptionInfo:(const BITCrashUncaughtCXXExceptionInfo *)info {
   extern char* __cxa_demangle(const char* mangled_name, char* output_buffer, size_t* length, int* status);
-  char *demangled_name = __cxa_demangle ? __cxa_demangle(info->exception_type_name ?: "", NULL, NULL, NULL) : NULL;
+  char *demangled_name = &__cxa_demangle ? __cxa_demangle(info->exception_type_name ?: "", NULL, NULL, NULL) : NULL;
 
   if ((self = [super
                 initWithName:[NSString stringWithUTF8String:demangled_name ?: info->exception_type_name ?: ""]
