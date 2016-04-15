602bc14f1be02f7af56c5237875e546f3d7c0215
Lukas Spieß <lspiess@microsoft.com>
2016-04-16 01:59:28 +0200

    Replace empty NSError in tests with proper dummy object

--- Support/HockeySDKTests/BITSenderTests.m
+++ Support/HockeySDKTests/BITSenderTests.m
@@ -176,7 +176,10 @@
   NSString *testFilePath = @"path/to/file";
   
   // test
-  [_sut handleResponseWithStatusCode:testStatusCode responseData:testData filePath:testFilePath error:[NSError new]];
+  [_sut handleResponseWithStatusCode:testStatusCode
+                        responseData:testData
+                            filePath:testFilePath
+                               error:[NSError errorWithDomain:@"Network error" code:503 userInfo:nil]];
   
   //verify
   [verify(_mockPersistence) giveBackRequestedFilePath:testFilePath];
