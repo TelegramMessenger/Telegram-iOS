70a67727b44f235bb632c7e98ee4e127c14ed58f
Lukas Spieß <lspiess@microsoft.com>
2016-04-18 03:00:29 +0200

    Add tests for BITHockeyLogger

---
+++ Support/HockeySDKTests/BITHockeyLoggerTests.m
@@ -0,0 +1,188 @@
+#import <XCTest/XCTest.h>
+#import "OCMock.h"
+#import "BITHockeyLogger.h"
+
+static char *const testFile = "Filename";
+static char *const testFunction = "Function name";
+static uint const testLine = 42;
+
+@interface BITHockeyLoggerTests : XCTestCase
+
+@end
+
+@implementation BITHockeyLoggerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testInitialLogLevel {
+  XCTAssertEqual([BITHockeyLogger currentLogLevel], BITLogLevelWarning);
+}
+
+- (void)testSetCurrentLogLevel {
+  [BITHockeyLogger setCurrentLogLevel:BITLogLevelNone];
+  XCTAssertEqual([BITHockeyLogger currentLogLevel], BITLogLevelNone);
+}
+
+- (void)testSetLogHandler {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"Expected logHandler to be called"];
+  BITLogHandler testLogHandler = ^(BITLogMessageProvider messageProvider, BITLogLevel logLevel, const char *file, const char *function, uint line) {
+    [expectation fulfill];
+  };
+
+  [BITHockeyLogger setLogHandler:testLogHandler];
+
+  [BITHockeyLogger logMessage:^{ return @"Test"; } level:BITLogLevelError file:testFile function:testFunction line:testLine];
+
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+}
+
+#pragma mark - Test Default LogHandler
+
+- (void)testLogLevelNone {
+  [BITHockeyLogger setCurrentLogLevel:BITLogLevelNone];
+
+  [BITHockeyLogger logMessage:^NSString * {
+    XCTFail(@"This should not be called");
+    return nil;
+  } level:BITLogLevelError file:testFile function:testFunction line:testLine];
+  
+  [BITHockeyLogger logMessage:^NSString * {
+    XCTFail(@"This should not be called");
+    return nil;
+  } level:BITLogLevelWarning file:testFile function:testFunction line:testLine];
+  
+  [BITHockeyLogger logMessage:^NSString * {
+    XCTFail(@"This should not be called");
+    return nil;
+  } level:BITLogLevelDebug file:testFile function:testFunction line:testLine];
+  
+  [BITHockeyLogger logMessage:^NSString * {
+    XCTFail(@"This should not be called");
+    return nil;
+  } level:BITLogLevelVerbose file:testFile function:testFunction line:testLine];
+}
+
+- (void)testLogLevelError {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"An error message should have been logged"];
+  [BITHockeyLogger setCurrentLogLevel:BITLogLevelError];
+  
+  [BITHockeyLogger logMessage:^NSString * {
+    [expectation fulfill];
+    return nil;
+  } level:BITLogLevelError file:testFile function:testFunction line:testLine];
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+  
+  [BITHockeyLogger logMessage:^NSString * {
+    XCTFail(@"This should not be called");
+    return nil;
+  } level:BITLogLevelWarning file:testFile function:testFunction line:testLine];
+  
+  [BITHockeyLogger logMessage:^NSString * {
+    XCTFail(@"This should not be called");
+    return nil;
+  } level:BITLogLevelDebug file:testFile function:testFunction line:testLine];
+  
+  [BITHockeyLogger logMessage:^NSString * {
+    XCTFail(@"This should not be called");
+    return nil;
+  } level:BITLogLevelVerbose file:testFile function:testFunction line:testLine];
+}
+
+- (void)testLogLevelWarning {
+  [BITHockeyLogger setCurrentLogLevel:BITLogLevelWarning];
+
+  XCTestExpectation *expectation1 = [self expectationWithDescription:@"An error message should have been logged"];
+  [BITHockeyLogger logMessage:^NSString * {
+    [expectation1 fulfill];
+    return nil;
+  } level:BITLogLevelError file:testFile function:testFunction line:testLine];
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+  
+  XCTestExpectation *expectation2 = [self expectationWithDescription:@"A warning message should have been logged"];
+  [BITHockeyLogger logMessage:^NSString * {
+    [expectation2 fulfill];
+    return nil;
+  } level:BITLogLevelWarning file:testFile function:testFunction line:testLine];
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+  
+  [BITHockeyLogger logMessage:^NSString * {
+    XCTFail(@"This should not be called");
+    return nil;
+  } level:BITLogLevelDebug file:testFile function:testFunction line:testLine];
+
+  [BITHockeyLogger logMessage:^NSString * {
+    XCTFail(@"This should not be called");
+    return nil;
+  } level:BITLogLevelVerbose file:testFile function:testFunction line:testLine];
+}
+
+- (void)testLogLevelDebug {
+  [BITHockeyLogger setCurrentLogLevel:BITLogLevelDebug];
+  
+  XCTestExpectation *expectation1 = [self expectationWithDescription:@"An error message should have been logged"];
+  [BITHockeyLogger logMessage:^NSString * {
+    [expectation1 fulfill];
+    return nil;
+  } level:BITLogLevelError file:testFile function:testFunction line:testLine];
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+  
+  XCTestExpectation *expectation2 = [self expectationWithDescription:@"A warning message should have been logged"];
+  [BITHockeyLogger logMessage:^NSString * {
+    [expectation2 fulfill];
+    return nil;
+  } level:BITLogLevelWarning file:testFile function:testFunction line:testLine];
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+
+  XCTestExpectation *expectation3 = [self expectationWithDescription:@"A debug message should have been logged"];
+  [BITHockeyLogger logMessage:^NSString * {
+    [expectation3 fulfill];
+    return nil;
+  } level:BITLogLevelDebug file:testFile function:testFunction line:testLine];
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+
+  [BITHockeyLogger logMessage:^NSString * {
+    XCTFail(@"This should not be called");
+    return nil;
+  } level:BITLogLevelVerbose file:testFile function:testFunction line:testLine];
+}
+
+- (void)testLogLevelVerbose {
+  [BITHockeyLogger setCurrentLogLevel:BITLogLevelVerbose];
+  
+  XCTestExpectation *expectation1 = [self expectationWithDescription:@"An error message should have been logged"];
+  [BITHockeyLogger logMessage:^NSString * {
+    [expectation1 fulfill];
+    return nil;
+  } level:BITLogLevelError file:testFile function:testFunction line:testLine];
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+  
+  XCTestExpectation *expectation2 = [self expectationWithDescription:@"A warning message should have been logged"];
+  [BITHockeyLogger logMessage:^NSString * {
+    [expectation2 fulfill];
+    return nil;
+  } level:BITLogLevelWarning file:testFile function:testFunction line:testLine];
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+
+  XCTestExpectation *expectation3 = [self expectationWithDescription:@"A debug message should have been logged"];
+  [BITHockeyLogger logMessage:^NSString * {
+    [expectation3 fulfill];
+    return nil;
+  } level:BITLogLevelDebug file:testFile function:testFunction line:testLine];
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+
+  XCTestExpectation *expectation4 = [self expectationWithDescription:@"A verbose message should have been logged"];
+  [BITHockeyLogger logMessage:^NSString * {
+    [expectation4 fulfill];
+    return nil;
+  } level:BITLogLevelVerbose file:testFile function:testFunction line:testLine];
+  [self waitForExpectationsWithTimeout:0 handler:nil];
+}
+@end
