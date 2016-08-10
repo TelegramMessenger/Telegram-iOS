f5a63ed4abc41cc1ec3696d59e0dd4b34849eb41
Peter <iakovlev.peter.p@gmail.com>
2016-08-10 19:31:50 +0300

    Initial Commit

---
+++ TelegramUI/TelegramUI.h
@@ -0,0 +1,19 @@
+//
+//  TelegramUI.h
+//  TelegramUI
+//
+//  Created by Peter on 8/10/16.
+//  Copyright © 2016 Telegram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TelegramUI.
+FOUNDATION_EXPORT double TelegramUIVersionNumber;
+
+//! Project version string for TelegramUI.
+FOUNDATION_EXPORT const unsigned char TelegramUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TelegramUI/PublicHeader.h>
+
+
