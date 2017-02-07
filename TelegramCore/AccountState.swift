826d1523b7b4ad1aaa62560ccc9cb000e1bf4a1d
Peter <iakovlev.peter.p@gmail.com>
2017-02-07 22:50:27 +0300

    no message

--- TelegramCore/AccountState.swift
+++ TelegramCore/AccountState.swift
@@ -1,5 +1,9 @@
 import Foundation
-import Postbox
+#if os(macOS)
+    import PostboxMac
+#else
+    import Postbox
+#endif
 
 private enum SentAuthorizationCodeTypeValue: Int32 {
     case otherSession = 0
