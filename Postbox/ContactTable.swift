c3e29b7dcdf4ebe3191dd3cd86c5c1e7909818ed
Peter <iakovlev.peter.p@gmail.com>
2017-03-06 01:36:17 +0300

    no message

--- Postbox/ContactTable.swift
+++ Postbox/ContactTable.swift
@@ -5,11 +5,16 @@ final class ContactTable: Table {
         return ValueBoxTable(id: id, keyType: .binary)
     }
     
-    private var originalPeerIds: Set<PeerId>?
+    private let peerNameIndexTable: PeerNameIndexTable
+    
+    private var peerIdsBeforeModification: Set<PeerId>?
     private var peerIds: Set<PeerId>?
     
-    private var addedPeerIds = Set<PeerId>()
-    private var removedPeerIds = Set<PeerId>()
+    init(valueBox: ValueBox, table: ValueBoxTable, peerNameIndexTable: PeerNameIndexTable) {
+        self.peerNameIndexTable = peerNameIndexTable
+        
+        super.init(valueBox: valueBox, table: table)
+    }
     
     private func key(_ id: PeerId, sharedKey: ValueBoxKey = ValueBoxKey(length: 8)) -> ValueBoxKey {
         sharedKey.setInt64(0, value: id.toInt64())
@@ -25,18 +30,7 @@ final class ContactTable: Table {
     }
     
     func isContact(peerId: PeerId) -> Bool {
-        if let peerIds = self.peerIds {
-            return peerIds.contains(peerId)
-        } else {
-            var peerIds = Set<PeerId>()
-            self.valueBox.range(self.table, start: self.lowerBound(), end: self.upperBound(), keys: { key in
-                peerIds.insert(PeerId(key.getInt64(0)))
-                return true
-            }, limit: 0)
-            self.peerIds = peerIds
-            self.originalPeerIds = peerIds
-            return peerIds.contains(peerId)
-        }
+        return self.get().contains(peerId)
     }
     
     func get() -> Set<PeerId> {
@@ -49,49 +43,45 @@ final class ContactTable: Table {
                 return true
             }, limit: 0)
             self.peerIds = peerIds
-            self.originalPeerIds = peerIds
             return peerIds
         }
     }
     
     func replace(_ ids: Set<PeerId>) {
-        let currentPeerIds = self.get()
-        
-        let previousPeerIds: Set<PeerId>
-        if let originalPeerIds = self.originalPeerIds {
-            previousPeerIds = originalPeerIds
-        } else {
-            previousPeerIds = currentPeerIds
-            self.originalPeerIds = currentPeerIds
+        if self.peerIdsBeforeModification == nil {
+           self.peerIdsBeforeModification = self.get()
         }
-        
-        self.removedPeerIds = previousPeerIds.subtracting(ids)
-        self.addedPeerIds = ids.subtracting(previousPeerIds)
         
         self.peerIds = ids
     }
     
     override func clearMemoryCache() {
-        self.originalPeerIds = nil
+        assert(self.peerIdsBeforeModification == nil)
         self.peerIds = nil
-        self.addedPeerIds.removeAll()
-        self.removedPeerIds.removeAll()
     }
     
     override func beforeCommit() {
-        let sharedKey = self.key(PeerId(namespace: 0, id: 0))
-        
-        for peerId in self.removedPeerIds {
-            self.valueBox.remove(self.table, key: self.key(peerId, sharedKey: sharedKey))
+        if let peerIdsBeforeModification = self.peerIdsBeforeModification {
+            if let peerIds = self.peerIds {
+                let removedPeerIds = peerIdsBeforeModification.subtracting(peerIds)
+                let addedPeerIds = peerIds.subtracting(peerIdsBeforeModification)
+                
+                let sharedKey = self.key(PeerId(namespace: 0, id: 0))
+                
+                for peerId in removedPeerIds {
+                    self.valueBox.remove(self.table, key: self.key(peerId, sharedKey: sharedKey))
+                    self.peerNameIndexTable.setPeerCategoryState(peerId: peerId, category: [.contacts], includes: false)
+                }
+                
+                for peerId in addedPeerIds {
+                    self.valueBox.set(self.table, key: self.key(peerId, sharedKey: sharedKey), value: MemoryBuffer())
+                    self.peerNameIndexTable.setPeerCategoryState(peerId: peerId, category: [.contacts], includes: true)
+                }
+            } else {
+                assertionFailure()
+            }
+            
+            self.peerIdsBeforeModification = nil
         }
-        
-        for peerId in self.addedPeerIds {
-            self.valueBox.set(self.table, key: self.key(peerId, sharedKey: sharedKey), value: MemoryBuffer())
-        }
-        
-        self.originalPeerIds = self.peerIds
-        
-        self.addedPeerIds.removeAll()
-        self.removedPeerIds.removeAll()
     }
 }
