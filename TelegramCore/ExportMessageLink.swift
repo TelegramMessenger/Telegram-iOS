ae9f0035fc2a860063e9ff0e96f9fc5090645b95
overtake <overtakeful@gmail.com>
2017-05-02 14:36:42 +0300

    sidebar

---
+++ TelegramCore/ExportMessageLink.swift
@@ -0,0 +1,28 @@
+
+#if os(macOS)
+    import PostboxMac
+    import SwiftSignalKitMac
+#else
+    import Postbox
+    import SwiftSignalKit
+#endif
+
+
+public func exportMessageLink(account:Account, peerId:PeerId, messageId:MessageId) -> Signal<String?, Void> {
+    return account.postbox.modify { modifier -> Peer? in
+        return modifier.getPeer(peerId)
+        } |> mapToSignal { peer -> Signal<String?, Void> in
+            if let peer = peer, let input = apiInputChannel(peer) {
+                return account.network.request(Api.functions.channels.exportMessageLink(channel: input, id: messageId.id)) |> mapError {_ in return } |> map { res in
+                    switch res {
+                    case .exportedMessageLink(let link):
+                        return link
+                    }
+                    } |> `catch` { _ -> Signal<String?, NoError> in
+                        return .single(nil)
+                }
+            } else {
+                return .single(nil)
+            }
+    }
+}
