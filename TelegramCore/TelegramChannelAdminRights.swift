29826304da68627132891a947757662b03e68ad8
overtake <overtakeful@gmail.com>
2017-06-29 17:51:15 +0300

    no message

--- TelegramCore/TelegramChannelAdminRights.swift
+++ TelegramCore/TelegramChannelAdminRights.swift
@@ -38,6 +38,7 @@ public struct TelegramChannelAdminRightsFlags: OptionSet {
     
     public static var broadcastSpecific: TelegramChannelAdminRightsFlags = [
         .canChangeInfo,
+        .canInviteUsers,
         .canPostMessages,
         .canEditMessages,
         .canDeleteMessages,
