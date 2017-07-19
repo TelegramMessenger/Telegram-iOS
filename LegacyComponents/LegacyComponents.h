138469c9fd69fe5bcb880d3866329e0866301d95
Peter <iakovlev.peter.p@gmail.com>
2017-07-19 15:58:59 +0300

    Initial Commit

---
+++ LegacyComponents/LegacyComponents.h
@@ -0,0 +1,19 @@
+//
+//  LegacyComponents.h
+//  LegacyComponents
+//
+//  Created by Peter on 7/19/17.
+//  Copyright © 2017 Telegram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LegacyComponents.
+FOUNDATION_EXPORT double LegacyComponentsVersionNumber;
+
+//! Project version string for LegacyComponents.
+FOUNDATION_EXPORT const unsigned char LegacyComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LegacyComponents/PublicHeader.h>
+
+
