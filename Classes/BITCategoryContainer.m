c8f3bf8e6d99da798253b6b7bb2c673969ca6e1a
Murat Baysangurov <v-mubays@microsoft.com>
2017-07-27 15:01:58 +0300

    Fixed “cast drops const qualifier” warnings

--- Classes/BITCategoryContainer.m
+++ Classes/BITCategoryContainer.m
@@ -63,7 +63,10 @@ static const NSUInteger ChunkSize = 16384;
     stream.zfree = Z_NULL;
     stream.opaque = Z_NULL;
     stream.avail_in = (uint)[self length];
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcast-qual"
     stream.next_in = (Bytef *)[self bytes];
+#pragma clang diagnostic pop
     stream.total_out = 0;
     stream.avail_out = 0;
     
@@ -102,7 +105,10 @@ static const NSUInteger ChunkSize = 16384;
     stream.zalloc = Z_NULL;
     stream.zfree = Z_NULL;
     stream.avail_in = (uint)[self length];
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcast-qual"
     stream.next_in = (Bytef *)[self bytes];
+#pragma clang diagnostic pop
     stream.total_out = 0;
     stream.avail_out = 0;
     
