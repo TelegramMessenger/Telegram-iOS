c8ad9bcecc8c3b8fff7d82e76f08478c82bfb4b4
Dave Weston <dweston@microsoft.com>
2017-10-16 12:34:45 -0700

    Detect memory pressure in extensions
    The UIApplicationDidReceiveMemoryWarningNotification isn't fired when
    running in an extension, so we need to resort to a lower-level mechanism.

--- Classes/BITCrashManager.m
+++ Classes/BITCrashManager.m
@@ -545,17 +545,30 @@ __attribute__((noreturn)) static void uncaught_cxx_exception_handler(const BITCr
   }
   
   if (nil == self.appDidReceiveLowMemoryWarningObserver) {
-    self.appDidReceiveLowMemoryWarningObserver = [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidReceiveMemoryWarningNotification
-                                                                                                   object:nil
-                                                                                                    queue:NSOperationQueue.mainQueue
-                                                                                               usingBlock:^(NSNotification __unused *note) {
-                                                                                                 // we only need to log this once
-                                                                                                 if (!self.didLogLowMemoryWarning) {
-                                                                                                   [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kBITAppDidReceiveLowMemoryNotification];
-                                                                                                   self.didLogLowMemoryWarning = YES;
-                                                                                                   
-                                                                                                 }
-                                                                                               }];
+    if (bit_isRunningInAppExtension()) {
+      static dispatch_once_t onceToken;
+      dispatch_once(&onceToken, ^{
+        dispatch_source_t source = dispatch_source_create(DISPATCH_SOURCE_TYPE_MEMORYPRESSURE, 0, DISPATCH_MEMORYPRESSURE_WARN|DISPATCH_MEMORYPRESSURE_CRITICAL, dispatch_get_main_queue());
+        dispatch_source_set_event_handler(source, ^{
+          if (!self.didLogLowMemoryWarning) {
+            [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kBITAppDidReceiveLowMemoryNotification];
+            self.didLogLowMemoryWarning = YES;
+          }
+        });
+      });
+    } else {
+      self.appDidReceiveLowMemoryWarningObserver = [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidReceiveMemoryWarningNotification
+                                                                                                     object:nil
+                                                                                                      queue:NSOperationQueue.mainQueue
+                                                                                                 usingBlock:^(NSNotification __unused *note) {
+                                                                                                   // we only need to log this once
+                                                                                                   if (!self.didLogLowMemoryWarning) {
+                                                                                                     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kBITAppDidReceiveLowMemoryNotification];
+                                                                                                     self.didLogLowMemoryWarning = YES;
+
+                                                                                                   }
+                                                                                                 }];
+    }
   }
 }
 
