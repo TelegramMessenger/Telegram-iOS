139fb8fc7c835240e3d0b0598db49e88915f0497
Peter Iakovlev <iakovlev.peter.p@gmail.com>
2018-03-03 11:21:09 +0400

    no message

--- Display/ImmediateTextNode.swift
+++ Display/ImmediateTextNode.swift
@@ -3,10 +3,12 @@ import Foundation
 public final class ImmediateTextNode: TextNode {
     public var attributedText: NSAttributedString?
     public var textAlignment: NSTextAlignment = .natural
+    public var maximumNumberOfLines: Int = 1
+    public var lineSpacing: CGFloat = 0.0
     
     public func updateLayout(_ constrainedSize: CGSize) -> CGSize {
         let makeLayout = TextNode.asyncLayout(self)
-        let (layout, apply) = makeLayout(TextNodeLayoutArguments(attributedString: self.attributedText, backgroundColor: nil, maximumNumberOfLines: 1, truncationType: .end, constrainedSize: constrainedSize, alignment: self.textAlignment, lineSpacing: 0.0, cutout: nil, insets: UIEdgeInsets()))
+        let (layout, apply) = makeLayout(TextNodeLayoutArguments(attributedString: self.attributedText, backgroundColor: nil, maximumNumberOfLines: self.maximumNumberOfLines, truncationType: .end, constrainedSize: constrainedSize, alignment: self.textAlignment, lineSpacing: self.lineSpacing, cutout: nil, insets: UIEdgeInsets()))
         let _ = apply()
         return layout.size
     }
