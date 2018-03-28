e91ca3f86bd2efebe4d879a67b848da934550fcc
Mikhail Filimonov <telegram@Mikhails-iMac-Pro.local>
2018-03-28 22:13:42 +0400

    no message

--- TelegramCore/SecureFileMediaResource.swift
+++ TelegramCore/SecureFileMediaResource.swift
@@ -1,5 +1,9 @@
 import Foundation
-import Postbox
+#if os(macOS)
+    import PostboxMac
+#else
+    import Postbox
+#endif
 
 public struct SecureFileMediaResourceId: MediaResourceId {
     let fileId: Int64
