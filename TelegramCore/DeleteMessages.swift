d47a174cd8407ff7673edf64f5bfaa67f3b86b00
Peter <iakovlev.peter.p@gmail.com>
2018-07-20 13:46:18 +0300

    no message

--- TelegramCore/DeleteMessages.swift
+++ TelegramCore/DeleteMessages.swift
@@ -16,7 +16,7 @@ public func deleteMessages(transaction: Transaction, mediaBox: MediaBox, ids: [M
                         let _ = mediaBox.removeCachedResources(Set(image.representations.map({ WrappedMediaResourceId($0.resource.id) }))).start()
                     } else if let file = media as? TelegramMediaFile {
                         let _ = mediaBox.removeCachedResources(Set(file.previewRepresentations.map({ WrappedMediaResourceId($0.resource.id) }))).start()
-                        let _ = mediaBox.removeCachedResources(Set([WrappedMediaResourceId(file.resource.id)]))
+                        let _ = mediaBox.removeCachedResources(Set([WrappedMediaResourceId(file.resource.id)])).start()
                     }
                 }
             }
