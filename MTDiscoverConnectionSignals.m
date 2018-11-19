1f7f9349e57ddb544c9e50de5be053f8fde83bc8
Peter <>
2018-11-19 15:21:20 +0300

    no message

--- MTDiscoverConnectionSignals.m
+++ MTDiscoverConnectionSignals.m
@@ -264,6 +264,7 @@
                     MTDatacenterAddress *portAddress = [[MTDatacenterAddress alloc] initWithIp:address.ip port:[nPort intValue] preferForMedia:address.preferForMedia restrictToTcp:address.restrictToTcp cdn:address.cdn preferForProxy:address.preferForProxy secret:address.secret];
                     MTTransportScheme *tcpPortTransportScheme = [[MTTransportScheme alloc] initWithTransportClass:[MTTcpTransport class] address:portAddress media:media];
                     MTSignal *tcpConnectionWithTimeout = [[[self tcpConnectionWithContext:context datacenterId:datacenterId address:portAddress] then:[MTSignal single:tcpPortTransportScheme]] timeout:5.0 onQueue:[MTQueue concurrentDefaultQueue] orSignal:[MTSignal fail:nil]];
+                    tcpConnectionWithTimeout = [tcpConnectionWithTimeout delay:5.0 onQueue:[MTQueue concurrentDefaultQueue]];
                     MTSignal *signal = [tcpConnectionWithTimeout catch:^MTSignal *(__unused id error) {
                         return [MTSignal complete];
                     }];
