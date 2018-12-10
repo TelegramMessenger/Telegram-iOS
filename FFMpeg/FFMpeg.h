429b8d43b9a5190006fb58365411274d7844b3f0
Peter Iakovlev <iakovlev.peter.p@gmail.com>
2018-12-11 01:13:54 +0400

    Initial Commit

---
+++ FFMpeg/FFMpeg.h
@@ -0,0 +1,19 @@
+//
+//  FFMpeg.h
+//  FFMpeg
+//
+//  Created by Peter Iakovlev on 11/12/2018.
+//  Copyright © 2018 Telegram Messenger LLP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FFMpeg.
+FOUNDATION_EXPORT double FFMpegVersionNumber;
+
+//! Project version string for FFMpeg.
+FOUNDATION_EXPORT const unsigned char FFMpegVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFMpeg/PublicHeader.h>
+
+
