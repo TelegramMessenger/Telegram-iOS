0a325b079e9926cfa77dabeaea3e632a6fe574a5
Hermet Park <hermetpark@gmail.com>
2018-12-26 19:18:29 +0900

    Put samsung copyright header to all our own source code.
    Change-Id: I3cebcfef827425d16092ee7a26c39f7733572eb7

--- src/vector/vpainter.h
+++ src/vector/vpainter.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://floralicense.org/license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef VPAINTER_H
 #define VPAINTER_H
 
