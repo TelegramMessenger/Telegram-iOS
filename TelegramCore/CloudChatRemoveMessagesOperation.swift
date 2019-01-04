09500cf1bf2bcea8991a83b7fba31ce6612d575d
Peter <>
2019-01-04 22:34:51 +0100

    Added API for deleting channels

--- TelegramCore/CloudChatRemoveMessagesOperation.swift
+++ TelegramCore/CloudChatRemoveMessagesOperation.swift
@@ -46,17 +46,20 @@ final class CloudChatRemoveMessagesOperation: PostboxCoding {
 final class CloudChatRemoveChatOperation: PostboxCoding {
     let peerId: PeerId
     let reportChatSpam: Bool
+    let deleteGloballyIfPossible: Bool
     let topMessageId: MessageId?
     
-    init(peerId: PeerId, reportChatSpam: Bool, topMessageId: MessageId?) {
+    init(peerId: PeerId, reportChatSpam: Bool, deleteGloballyIfPossible: Bool, topMessageId: MessageId?) {
         self.peerId = peerId
         self.reportChatSpam = reportChatSpam
+        self.deleteGloballyIfPossible = deleteGloballyIfPossible
         self.topMessageId = topMessageId
     }
     
     init(decoder: PostboxDecoder) {
         self.peerId = PeerId(decoder.decodeInt64ForKey("p", orElse: 0))
         self.reportChatSpam = decoder.decodeInt32ForKey("r", orElse: 0) != 0
+        self.deleteGloballyIfPossible = decoder.decodeInt32ForKey("deleteGloballyIfPossible", orElse: 0) != 0
         if let messageIdPeerId = decoder.decodeOptionalInt64ForKey("m.p"), let messageIdNamespace = decoder.decodeOptionalInt32ForKey("m.n"), let messageIdId = decoder.decodeOptionalInt32ForKey("m.i") {
             self.topMessageId = MessageId(peerId: PeerId(messageIdPeerId), namespace: messageIdNamespace, id: messageIdId)
         } else {
@@ -67,6 +70,7 @@ final class CloudChatRemoveChatOperation: PostboxCoding {
     func encode(_ encoder: PostboxEncoder) {
         encoder.encodeInt64(self.peerId.toInt64(), forKey: "p")
         encoder.encodeInt32(self.reportChatSpam ? 1 : 0, forKey: "r")
+        encoder.encodeInt32(self.deleteGloballyIfPossible ? 1 : 0, forKey: "deleteGloballyIfPossible")
         if let topMessageId = self.topMessageId {
             encoder.encodeInt64(topMessageId.peerId.toInt64(), forKey: "m.p")
             encoder.encodeInt32(topMessageId.namespace, forKey: "m.n")
@@ -105,8 +109,8 @@ func cloudChatAddRemoveMessagesOperation(transaction: Transaction, peerId: PeerI
     transaction.operationLogAddEntry(peerId: peerId, tag: OperationLogTags.CloudChatRemoveMessages, tagLocalIndex: .automatic, tagMergedIndex: .automatic, contents: CloudChatRemoveMessagesOperation(messageIds: messageIds, type: type))
 }
 
-func cloudChatAddRemoveChatOperation(transaction: Transaction, peerId: PeerId, reportChatSpam: Bool) {
-    transaction.operationLogAddEntry(peerId: peerId, tag: OperationLogTags.CloudChatRemoveMessages, tagLocalIndex: .automatic, tagMergedIndex: .automatic, contents: CloudChatRemoveChatOperation(peerId: peerId, reportChatSpam: reportChatSpam, topMessageId: transaction.getTopPeerMessageId(peerId: peerId, namespace: Namespaces.Message.Cloud)))
+func cloudChatAddRemoveChatOperation(transaction: Transaction, peerId: PeerId, reportChatSpam: Bool, deleteGloballyIfPossible: Bool) {
+    transaction.operationLogAddEntry(peerId: peerId, tag: OperationLogTags.CloudChatRemoveMessages, tagLocalIndex: .automatic, tagMergedIndex: .automatic, contents: CloudChatRemoveChatOperation(peerId: peerId, reportChatSpam: reportChatSpam, deleteGloballyIfPossible: deleteGloballyIfPossible, topMessageId: transaction.getTopPeerMessageId(peerId: peerId, namespace: Namespaces.Message.Cloud)))
 }
 
 func cloudChatAddClearHistoryOperation(transaction: Transaction, peerId: PeerId, explicitTopMessageId: MessageId?) {
