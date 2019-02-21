fd110c3419d1c06baa32c721d4e3a749a2c2aee8
Hermet Park <hermetpark@gmail.com>
2019-02-21 10:58:44 +0900

    vector stb: check oom for safety.

--- src/vector/stb/stb_image.h
+++ src/vector/stb/stb_image.h
@@ -3771,6 +3771,10 @@ static int stbi__jpeg_info(stbi__context *s, int *x, int *y, int *comp)
 {
    int result;
    stbi__jpeg* j = (stbi__jpeg*) (stbi__malloc(sizeof(stbi__jpeg)));
+   if (!j) {
+      stbi__errpuc("outofmem", "Out of memory");
+      return 0;
+   }
    j->s = s;
    result = stbi__jpeg_info_raw(j, x, y, comp);
    STBI_FREE(j);
