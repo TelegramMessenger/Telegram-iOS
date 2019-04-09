11b3a56ae271a55931ae0bccb8b3cf2b8f3ae191
Peter <>
2019-04-09 21:08:40 +0100

    Added library

--- WebP/WebP.h
+++ WebP/WebP.h
@@ -1,11 +1,3 @@
-//
-//  WebP.h
-//  WebP
-//
-//  Created by Peter on 4/9/19.
-//  Copyright © 2019 Telegram LLP. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for WebP.
@@ -14,6 +6,4 @@ FOUNDATION_EXPORT double WebPVersionNumber;
 //! Project version string for WebP.
 FOUNDATION_EXPORT const unsigned char WebPVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <WebP/PublicHeader.h>
-
-
+#import <WebP/UIImage+WebP.h>
