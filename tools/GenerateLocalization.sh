7141d85bd72c718df61f856bd44987e85febdef3
Peter <>
2019-05-10 17:08:35 +0200

    Update submodules

---
+++ tools/GenerateLocalization.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+swift tools/GenerateLocalization.swift Telegram-iOS/en.lproj/Localizable.strings submodules/TelegramUI/TelegramUI/PresentationStrings.swift submodules/TelegramUI/TelegramUI/Resources/PresentationStrings.mapping
