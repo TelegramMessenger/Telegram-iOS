0943472607277696b1f41420853d336fc22b1cbe
Peter <>
2019-05-15 21:23:35 +0200

    FetchChatList: begin with the exact message id Serialization: redact phone numbers from logs

--- TelegramCore/Serialization.swift
+++ TelegramCore/Serialization.swift
@@ -19,7 +19,11 @@ private let redactChildrenOfType: [String: Set<String>] = [
     "Updates.updateShortChatMessage": Set(["message"]),
     "BotInlineMessage.botInlineMessageText": Set(["message"]),
     "DraftMessage.draftMessage": Set(["message"]),
-    "InputSingleMedia.inputSingleMedia": Set(["message"])
+    "InputSingleMedia.inputSingleMedia": Set(["message"]),
+    "inputPhoneContact": Set(["phone"]),
+    "user": Set(["phone"]),
+    "updateUserPhone": Set(["phone"]),
+    "inputPhoneContact": Set(["phone"])
 ]
 
 private let redactFunctionParameters: [String: Set<String>] = [
