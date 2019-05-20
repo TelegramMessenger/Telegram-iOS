b88bc7905f3ec39533c23d2f85adf5a94eddcfcb
Peter <>
2019-05-20 19:01:59 +0200

    Update project

--- Telegram-iOS/main.m
+++ Telegram-iOS/main.m
@@ -1,6 +1,15 @@
 #import <UIKit/UIKit.h>
+#import <dlfcn.h>
 
 int main(int argc, char *argv[]) {
+    /*NSString *basePath = [[NSString stringWithUTF8String:argv[0]] stringByDeletingLastPathComponent];
+    void *Share = dlopen([[basePath stringByAppendingPathComponent:@"PlugIns/Share.appex/Share"] UTF8String], RTLD_LAZY);
+    void *NotificationContent = dlopen([[basePath stringByAppendingPathComponent:@"PlugIns/NotificationContent.appex/NotificationContent"] UTF8String], RTLD_LAZY);
+    sleep(1000);
+    void *NotificationService = dlopen([[basePath stringByAppendingPathComponent:@"PlugIns/NotificationService.appex/NotificationService"] UTF8String], RTLD_LAZY);
+    void *SiriIntents = dlopen([[basePath stringByAppendingPathComponent:@"PlugIns/SiriIntents.appex/SiriIntents"] UTF8String], RTLD_LAZY);
+    void *Widget = dlopen([[basePath stringByAppendingPathComponent:@"PlugIns/Widget.appex/Widget"] UTF8String], RTLD_LAZY);*/
+    
     @autoreleasepool {
         return UIApplicationMain(argc, argv, @"Application", @"AppDelegate");
     }
