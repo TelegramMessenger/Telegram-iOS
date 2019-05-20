ec5a99fbbe7aef512cd1ae214551f7d5cc22d8e0
Ilya Laktyushin <ilya@laktyush.in>
2019-05-20 02:49:06 +0200

    Added URL Auth support

--- TelegramCore/RequestMessageActionCallback.swift
+++ TelegramCore/RequestMessageActionCallback.swift
@@ -59,3 +59,70 @@ public func requestMessageActionCallback(account: Account, messageId: MessageId,
             }
         }
 }
+
+public enum MessageActionUrlAuthResult {
+    case `default`
+    case accepted(String)
+    case request(String, Peer, Bool)
+}
+
+
+public func requestMessageActionUrlAuth(account: Account, messageId: MessageId, buttonId: Int32) -> Signal<MessageActionUrlAuthResult, NoError> {
+    return account.postbox.loadedPeerWithId(messageId.peerId)
+    |> take(1)
+    |> mapToSignal { peer in
+        if let inputPeer = apiInputPeer(peer) {
+            return account.network.request(Api.functions.messages.requestUrlAuth(peer: inputPeer, msgId: messageId.id, buttonId: buttonId))
+            |> map(Optional.init)
+            |> `catch` { _ -> Signal<Api.UrlAuthResult?, NoError> in
+                return .single(nil)
+            }
+            |> map { result -> MessageActionUrlAuthResult in
+                guard let result = result else {
+                    return .default
+                }
+                switch result {
+                    case .urlAuthResultDefault:
+                        return .default
+                    case let .urlAuthResultAccepted(url):
+                        return .accepted(url)
+                    case let .urlAuthResultRequest(flags, bot, domain):
+                        return .request(domain, TelegramUser(user: bot), (flags & (1 << 0)) != 0)
+                }
+            }
+        } else {
+            return .single(.default)
+        }
+    }
+}
+
+public func acceptMessageActionUrlAuth(account: Account, messageId: MessageId, buttonId: Int32, allowWriteAccess: Bool) -> Signal<MessageActionUrlAuthResult, NoError> {
+    return account.postbox.loadedPeerWithId(messageId.peerId)
+    |> take(1)
+    |> mapToSignal { peer in
+        if let inputPeer = apiInputPeer(peer) {
+            var flags: Int32 = 0
+            if allowWriteAccess {
+                flags |= Int32(1 << 0)
+            }
+            return account.network.request(Api.functions.messages.acceptUrlAuth(flags: flags, peer: inputPeer, msgId: messageId.id, buttonId: buttonId))
+            |> map(Optional.init)
+            |> `catch` { _ -> Signal<Api.UrlAuthResult?, NoError> in
+                return .single(nil)
+            }
+            |> map { result -> MessageActionUrlAuthResult in
+                guard let result = result else {
+                    return .default
+                }
+                switch result {
+                    case let .urlAuthResultAccepted(url):
+                        return .accepted(url)
+                    default:
+                        return .default
+                }
+            }
+        } else {
+            return .single(.default)
+        }
+    }
+}
