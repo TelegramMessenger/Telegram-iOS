07be3d742602f4cfe7c2a9c35826417bef4ef7a1
Peter <>
2019-07-09 19:41:12 +0400

    Improve layout

--- submodules/Display/Display/ViewController.swift
+++ submodules/Display/Display/ViewController.swift
@@ -65,7 +65,7 @@ open class ViewControllerPresentationArguments {
         return self.validLayout
     }
     
-    private let presentationContext: PresentationContext
+    public let presentationContext: PresentationContext
     
     public final var supportedOrientations: ViewControllerSupportedOrientations = ViewControllerSupportedOrientations(regularSize: .all, compactSize: .allButUpsideDown)
     public final var lockedOrientation: UIInterfaceOrientationMask?
@@ -81,6 +81,7 @@ open class ViewControllerPresentationArguments {
     
     public final var isOpaqueWhenInOverlay: Bool = false
     public final var blocksBackgroundWhenInOverlay: Bool = false
+    public final var automaticallyControlPresentationContextLayout: Bool = true
     
     public func combinedSupportedOrientations(currentOrientationToLock: UIInterfaceOrientationMask) -> ViewControllerSupportedOrientations {
         return self.supportedOrientations
@@ -332,7 +333,9 @@ open class ViewControllerPresentationArguments {
         
         self.updateNavigationBarLayout(layout, transition: transition)
         
-        self.presentationContext.containerLayoutUpdated(layout, transition: transition)
+        if self.automaticallyControlPresentationContextLayout {
+            self.presentationContext.containerLayoutUpdated(layout, transition: transition)
+        }
         
         if let scrollToTopView = self.scrollToTopView {
             scrollToTopView.frame = CGRect(x: 0.0, y: 0.0, width: layout.size.width, height: 10.0)
