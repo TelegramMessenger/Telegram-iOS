a7e1cd39c87b669e7b25cff6964427fcedaebb0e
subhransu mohanty <sub.mohanty@samsung.com>
2019-07-12 15:51:58 +0900

    build fix

--- inc/rlottiecommon.h
+++ inc/rlottiecommon.h
@@ -19,10 +19,6 @@
 #ifndef _RLOTTIE_COMMON_H_
 #define _RLOTTIE_COMMON_H_
 
-#ifdef LOT_EXPORT
-  #undef LOT_EXPORT
-#endif
-
 #if defined _WIN32 || defined __CYGWIN__
   #ifdef LOT_BUILD
     #define LOT_EXPORT __declspec(dllexport)
@@ -229,8 +225,5 @@ typedef struct LOTLayerNode {
 /**
  * @}
  */
-
-#undef LOT_EXPORT
-#define LOT_EXPORT
 
 #endif  // _RLOTTIE_COMMON_H_
