f58901cdae01f297a9eb1476b77d2f9fc60fb822
Peter <>
2019-08-03 00:36:53 +0300

    Regenerate localization

--- tools/GenerateLocalization.sh
+++ tools/GenerateLocalization.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-swift tools/GenerateLocalization.swift Telegram-iOS/en.lproj/Localizable.strings submodules/TelegramPresentationData/Sources/PresentationStrings.swift submodules/TelegramUI/TelegramUI/Resources/PresentationStrings.mapping
+swift -swift-version 4 tools/GenerateLocalization.swift Telegram-iOS/en.lproj/Localizable.strings submodules/TelegramPresentationData/Sources/PresentationStrings.swift submodules/TelegramUI/TelegramUI/Resources/PresentationStrings.mapping
