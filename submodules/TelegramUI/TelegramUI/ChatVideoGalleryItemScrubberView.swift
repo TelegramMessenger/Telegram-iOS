peter-iakovlev/TelegramUI

Continuous scrubbing support for videos

TelegramUI/ChatVideoGalleryItemScrubberView.swift
@@ -40,10 +40,11 @@ final class ChatVideoGalleryItemScrubberView: UIView {
         }
     }
     
-    var seek: (Double) -> Void = { _ in }
+    var seek: (Double, MediaSeekState) -> Void = { _, _ in }
     
     override init(frame: CGRect) {
         self.scrubberNode = MediaPlayerScrubbingNode(content: .standard(lineHeight: 5.0, lineCap: .round, scrubberHandle: .circle, backgroundColor: UIColor(white: 1.0, alpha: 0.42), foregroundColor: .white))
+        self.scrubberNode.allowsContinuousSeekUpdates = true
         
         self.leftTimestampNode = MediaPlayerTimeTextNode(textColor: .white)
         self.rightTimestampNode = MediaPlayerTimeTextNode(textColor: .white)
@@ -57,8 +58,8 @@ final class ChatVideoGalleryItemScrubberView: UIView {
         
         super.init(frame: frame)
         
-        self.scrubberNode.seek = { [weak self] timestamp in
-            self?.seek(timestamp)
+        self.scrubberNode.seek = { [weak self] timestamp, seekState in
+            self?.seek(timestamp, seekState)
         }
         
         self.scrubberNode.playerStatusUpdated = { [weak self] status in

TelegramUI/PlatformVideoContent.swift
@@ -300,7 +300,7 @@ private final class PlatformVideoContentNode: ASDisplayNode, UniversalVideoConte
         assert(Queue.mainQueue().isCurrent())
     }
     
-    func seek(_ timestamp: Double) {
+    func seek(_ timestamp: Double, seekState: MediaSeekState = .unknown) {
         assert(Queue.mainQueue().isCurrent())
         self.player.seek(to: CMTime(seconds: timestamp, preferredTimescale: 30))
     }

TelegramUI/SystemVideoContent.swift
@@ -249,7 +249,7 @@ private final class SystemVideoContentNode: ASDisplayNode, UniversalVideoContent
         assert(Queue.mainQueue().isCurrent())
     }
     
-    func seek(_ timestamp: Double) {
+    func seek(_ timestamp: Double, seekState: MediaSeekState = .unknown) {
         assert(Queue.mainQueue().isCurrent())
         self.playerItem.seek(to: CMTimeMake(Int64(timestamp) * 1000, 1000))
     }

TelegramMessenger/Telegram-iOS

submodules/TelegramUI/TelegramUI/ChatVideoGalleryItemScrubberView.swift
@@ -42,10 +42,11 @@ final class ChatVideoGalleryItemScrubberView: UIView {
         }
     }
     
-    var seek: (Double) -> Void = { _ in }
+    var seek: (Double, MediaScrubState) -> Void = { _, _ in }
     
     override init(frame: CGRect) {
         self.scrubberNode = MediaPlayerScrubbingNode(content: .standard(lineHeight: 5.0, lineCap: .round, scrubberHandle: .circle, backgroundColor: UIColor(white: 1.0, alpha: 0.42), foregroundColor: .white))
+        self.scrubberNode.allowsContinuousSeekUpdates = true
         
         self.leftTimestampNode = MediaPlayerTimeTextNode(textColor: .white)
         self.rightTimestampNode = MediaPlayerTimeTextNode(textColor: .white)
@@ -59,8 +60,8 @@ final class ChatVideoGalleryItemScrubberView: UIView {
         
         super.init(frame: frame)
         
-        self.scrubberNode.seek = { [weak self] timestamp in
-            self?.seek(timestamp)
+        self.scrubberNode.seek = { [weak self] timestamp, seekState in
+            self?.seek(timestamp, seekState)
         }
         
         self.scrubberNode.playerStatusUpdated = { [weak self] status in

submodules/TelegramUI/TelegramUI/PlatformVideoContent.swift
@@ -303,7 +303,7 @@ private final class PlatformVideoContentNode: ASDisplayNode, UniversalVideoConte
         assert(Queue.mainQueue().isCurrent())
     }
     
-    func seek(_ timestamp: Double) {
+    func seek(_ timestamp: Double, mediaScrubState: MediaScrubState = .unknown) {
         assert(Queue.mainQueue().isCurrent())
         self.player.seek(to: CMTime(seconds: timestamp, preferredTimescale: 30))
     }

submodules/TelegramUI/TelegramUI/SystemVideoContent.swift
@@ -251,7 +251,7 @@ private final class SystemVideoContentNode: ASDisplayNode, UniversalVideoContent
         assert(Queue.mainQueue().isCurrent())
     }
     
-    func seek(_ timestamp: Double) {
+    func seek(_ timestamp: Double, mediaScrubState: MediaScrubState = .unknown) {
         assert(Queue.mainQueue().isCurrent())
         self.playerItem.seek(to: CMTimeMake(Int64(timestamp) * 1000, 1000))
     }