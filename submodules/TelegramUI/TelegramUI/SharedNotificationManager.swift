47c4ab17a3216ebfef568c7bfee9a4467c2f2039
Peter <>
2019-08-23 02:44:09 +0400

    Don't read "PIC" tags as they might be too large

--- submodules/TelegramUI/TelegramUI/SharedNotificationManager.swift
+++ submodules/TelegramUI/TelegramUI/SharedNotificationManager.swift
@@ -21,6 +21,14 @@ private final class PollStateContext {
     }
 }
 
+private final class NotificationInfo {
+    let dict: [AnyHashable: Any]
+    
+    init(dict: [AnyHashable: Any]) {
+        self.dict = dict
+    }
+}
+
 public final class SharedNotificationManager {
     private let episodeId: UInt32
     private let application: UIApplication
@@ -35,7 +43,7 @@ public final class SharedNotificationManager {
     private var accountsAndKeys: [(Account, Bool, MasterNotificationKey)]?
     private var accountsAndKeysDisposable: Disposable?
     
-    private var encryptedNotifications: [[AnyHashable: Any]] = []
+    private var notifications: [NotificationInfo] = []
     
     private var pollStateContexts: [AccountRecordId: PollStateContext] = [:]
     
@@ -149,8 +157,8 @@ public final class SharedNotificationManager {
         }
     }
     
-    func addEncryptedNotification(_ dict: [AnyHashable: Any]) {
-        self.encryptedNotifications.append(dict)
+    func addNotification(_ dict: [AnyHashable: Any]) {
+        self.notifications.append(NotificationInfo(dict: dict))
         
         if self.accountsAndKeys != nil {
             self.process()
@@ -162,8 +170,8 @@ public final class SharedNotificationManager {
             return
         }
         var decryptedNotifications: [(Account, Bool, [AnyHashable: Any])] = []
-        for dict in self.encryptedNotifications {
-            if var encryptedPayload = dict["p"] as? String {
+        for notification in self.notifications {
+            if var encryptedPayload = notification.dict["p"] as? String {
                 encryptedPayload = encryptedPayload.replacingOccurrences(of: "-", with: "+")
                 encryptedPayload = encryptedPayload.replacingOccurrences(of: "_", with: "/")
                 while encryptedPayload.count % 4 != 0 {
@@ -181,7 +189,7 @@ public final class SharedNotificationManager {
                 }
             }
         }
-        self.encryptedNotifications.removeAll()
+        self.notifications.removeAll()
         
         for (account, isCurrent, payload) in decryptedNotifications {
             var redactedPayload = payload
@@ -206,7 +214,7 @@ public final class SharedNotificationManager {
             var isAnnouncement = false
             var isLocationPolling = false
             var notificationRequestId: NotificationManagedNotificationRequestId?
-            var isMutePolling = false
+            var shouldPollState = false
             var title: String = ""
             var body: String?
             var apnsSound: String?
@@ -234,7 +242,7 @@ public final class SharedNotificationManager {
                     } else if locKey == "GEO_LIVE_PENDING" {
                         isLocationPolling = true
                     } else if locKey == "MESSAGE_MUTED" {
-                        isMutePolling = true
+                        shouldPollState = true
                     } else if locKey == "MESSAGE_DELETED" {
                         var peerId: PeerId?
                         if let fromId = payload["from_id"] {
@@ -281,8 +289,6 @@ public final class SharedNotificationManager {
                     } else {
                         body = nil
                     }
-                } else {
-                    body = nil
                 }
             }
             
@@ -326,6 +332,8 @@ public final class SharedNotificationManager {
                 } else {
                     var peerId: PeerId?
                     
+                    shouldPollState = true
+                    
                     if let fromId = payload["from_id"] {
                         let fromIdValue = fromId as! NSString
                         peerId = PeerId(namespace: Namespaces.Peer.CloudUser, id: Int32(fromIdValue.intValue))
@@ -350,7 +358,7 @@ public final class SharedNotificationManager {
                         }
                         notificationRequestId = .globallyUniqueId(randomIdValue.longLongValue, peerId)
                     } else {
-                        isMutePolling = true
+                        shouldPollState = true
                     }
                 }
             } else if let _ = payload["max_id"] {
@@ -386,7 +394,7 @@ public final class SharedNotificationManager {
                 return
             }
             
-            if notificationRequestId != nil || isMutePolling || isCall {
+            if notificationRequestId != nil || shouldPollState || isCall {
                 if !self.inForeground || !isCurrent {
                     self.beginPollingState(account: account)
                 }
