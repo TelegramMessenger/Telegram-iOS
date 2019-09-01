6e8b3ae8c26ce8188291c8083af80f0d3b5176f1
John Preston <johnprestonmail@gmail.com>
2019-09-01 10:37:11 +0300

    Check that root layer was found.

--- src/lottie/lottieparser.cpp
+++ src/lottie/lottieparser.cpp
@@ -598,7 +598,7 @@ void LottieParserImpl::parseComposition()
         }
     }
 
-    if (comp->mVersion.empty()) {
+    if (comp->mVersion.empty() || !comp->mRootLayer) {
         // don't have a valid bodymovin header
         return;
     }
