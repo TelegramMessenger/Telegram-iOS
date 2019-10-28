148d85bba3944f912ccaed7e379ff226c0aee408
Ilya Laktyushin <ilya@laktyush.in>
2019-10-28 17:53:48 +0400

    Fix Date Picker text color

--- submodules/SettingsUI/Sources/Themes/ThemeAutoNightTimeSelectionActionSheet.swift
+++ submodules/SettingsUI/Sources/Themes/ThemeAutoNightTimeSelectionActionSheet.swift
@@ -99,12 +99,12 @@ private final class ThemeAutoNightTimeSelectionActionSheetItemNode: ActionSheetI
         self.valueChanged = valueChanged
         
         self.pickerView = UIDatePicker()
+        self.pickerView.setValue(theme.primaryTextColor, forKey: "textColor")
+        self.pickerView.datePickerMode = .countDownTimer
         self.pickerView.datePickerMode = .time
         self.pickerView.timeZone = TimeZone(secondsFromGMT: 0)
         self.pickerView.date = Date(timeIntervalSince1970: Double(currentValue))
         self.pickerView.locale = Locale.current
-        
-        self.pickerView.setValue(theme.primaryTextColor, forKey: "textColor")
         
         super.init(theme: theme)
         
