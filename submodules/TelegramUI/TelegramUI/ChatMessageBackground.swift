b2674c22ceea8a291946b052cc94c1ebb72a6359
Ali <>
2019-12-18 23:24:57 +0400

    Separate bubble background and border into different layers

--- submodules/TelegramUI/TelegramUI/ChatMessageBackground.swift
+++ submodules/TelegramUI/TelegramUI/ChatMessageBackground.swift
@@ -60,41 +60,51 @@ enum ChatMessageBackgroundType: Equatable {
 class ChatMessageBackground: ASDisplayNode {
     private(set) var type: ChatMessageBackgroundType?
     private var currentHighlighted: Bool?
+    private var hasWallpaper: Bool?
     private var graphics: PrincipalThemeEssentialGraphics?
     private var maskMode: Bool?
     private let imageNode: ASImageNode
+    private let outlineImageNode: ASImageNode
     
     override init() {
         self.imageNode = ASImageNode()
         self.imageNode.displaysAsynchronously = false
         self.imageNode.displayWithoutProcessing = true
         
+        self.outlineImageNode = ASImageNode()
+        self.outlineImageNode.displaysAsynchronously = false
+        self.outlineImageNode.displayWithoutProcessing = true
+        
         super.init()
         
         self.isUserInteractionEnabled = false
+        self.addSubnode(self.outlineImageNode)
         self.addSubnode(self.imageNode)
     }
     
     func updateLayout(size: CGSize, transition: ContainedViewLayoutTransition) {
         transition.updateFrame(node: self.imageNode, frame: CGRect(origin: CGPoint(), size: size).insetBy(dx: -1.0, dy: -1.0))
+        transition.updateFrame(node: self.outlineImageNode, frame: CGRect(origin: CGPoint(), size: size).insetBy(dx: -1.0, dy: -1.0))
     }
     
     func setMaskMode(_ maskMode: Bool) {
-        if let type = self.type, let highlighted = self.currentHighlighted, let graphics = self.graphics {
-            self.setType(type: type, highlighted: highlighted, graphics: graphics, maskMode: maskMode, transition: .immediate)
+        if let type = self.type, let hasWallpaper = self.hasWallpaper, let highlighted = self.currentHighlighted, let graphics = self.graphics {
+            self.setType(type: type, highlighted: highlighted, graphics: graphics, maskMode: maskMode, hasWallpaper: hasWallpaper, transition: .immediate)
         }
     }
     
-    func setType(type: ChatMessageBackgroundType, highlighted: Bool, graphics: PrincipalThemeEssentialGraphics, maskMode: Bool, transition: ContainedViewLayoutTransition) {
+    func setType(type: ChatMessageBackgroundType, highlighted: Bool, graphics: PrincipalThemeEssentialGraphics, maskMode: Bool, hasWallpaper: Bool, transition: ContainedViewLayoutTransition) {
         let previousType = self.type
-        if let currentType = previousType, currentType == type, self.currentHighlighted == highlighted, self.graphics === graphics, self.maskMode == maskMode {
+        if let currentType = previousType, currentType == type, self.currentHighlighted == highlighted, self.graphics === graphics, self.maskMode == maskMode, self.hasWallpaper == hasWallpaper {
             return
         }
         self.type = type
         self.currentHighlighted = highlighted
         self.graphics = graphics
+        self.hasWallpaper = hasWallpaper
         
         let image: UIImage?
+        
         switch type {
         case .none:
             image = nil
@@ -142,6 +152,51 @@ class ChatMessageBackground: ASDisplayNode {
             }
         }
         
+        let outlineImage: UIImage?
+        
+        if hasWallpaper {
+            switch type {
+            case .none:
+                outlineImage = nil
+            case let .incoming(mergeType):
+                switch mergeType {
+                case .None:
+                    outlineImage = graphics.chatMessageBackgroundIncomingOutlineImage
+                case let .Top(side):
+                    if side {
+                        outlineImage = graphics.chatMessageBackgroundIncomingMergedTopSideOutlineImage
+                    } else {
+                        outlineImage = graphics.chatMessageBackgroundIncomingMergedTopOutlineImage
+                    }
+                case .Bottom:
+                    outlineImage = graphics.chatMessageBackgroundIncomingMergedBottomOutlineImage
+                case .Both:
+                    outlineImage = graphics.chatMessageBackgroundIncomingMergedBothOutlineImage
+                case .Side:
+                    outlineImage = graphics.chatMessageBackgroundIncomingMergedSideOutlineImage
+                }
+            case let .outgoing(mergeType):
+                switch mergeType {
+                case .None:
+                    outlineImage = graphics.chatMessageBackgroundOutgoingOutlineImage
+                case let .Top(side):
+                    if side {
+                        outlineImage = graphics.chatMessageBackgroundOutgoingMergedTopSideOutlineImage
+                    } else {
+                        outlineImage = graphics.chatMessageBackgroundOutgoingMergedTopOutlineImage
+                    }
+                case .Bottom:
+                    outlineImage = graphics.chatMessageBackgroundOutgoingMergedBottomOutlineImage
+                case .Both:
+                    outlineImage = graphics.chatMessageBackgroundOutgoingMergedBothOutlineImage
+                case .Side:
+                    outlineImage = graphics.chatMessageBackgroundOutgoingMergedSideOutlineImage
+                }
+            }
+        } else {
+            outlineImage = nil
+        }
+        
         if let previousType = previousType, previousType != .none, type == .none {
             if transition.isAnimated {
                 let tempLayer = CALayer()
@@ -152,7 +207,7 @@ class ChatMessageBackground: ASDisplayNode {
                 tempLayer.contentsCenter = self.imageNode.layer.contentsCenter
                 
                 tempLayer.frame = self.bounds
-                self.layer.addSublayer(tempLayer)
+                self.layer.insertSublayer(tempLayer, above: self.imageNode.layer)
                 transition.updateAlpha(layer: tempLayer, alpha: 0.0, completion: { [weak tempLayer] _ in
                     tempLayer?.removeFromSuperlayer()
                 })
@@ -164,5 +219,6 @@ class ChatMessageBackground: ASDisplayNode {
         }
         
         self.imageNode.image = image
+        self.outlineImageNode.image = outlineImage
     }
 }
