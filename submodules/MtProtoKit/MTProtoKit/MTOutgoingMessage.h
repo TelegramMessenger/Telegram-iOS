8f1dafed6e4ee9fcdf6d000e91e3e8e2adbba813
Ali <>
2020-01-02 01:32:14 +0400

    Fix message dependency ordering

--- submodules/MtProtoKit/MTProtoKit/MTOutgoingMessage.h
+++ submodules/MtProtoKit/MTProtoKit/MTOutgoingMessage.h
@@ -7,6 +7,7 @@
 @property (nonatomic, strong, readonly) id internalId;
 @property (nonatomic, strong, readonly) NSData *data;
 @property (nonatomic, strong, readonly) id metadata;
+@property (nonatomic, strong, readonly) NSString *additionalDebugDescription;
 @property (nonatomic, strong, readonly) id shortMetadata;
 @property (nonatomic, readonly) int64_t messageId;
 @property (nonatomic, readonly) int32_t messageSeqNo;
@@ -15,9 +16,9 @@
 @property (nonatomic) bool hasHighPriority;
 @property (nonatomic) int64_t inResponseToMessageId;
 
-@property (nonatomic, copy) id (^dynamicDecorator)(NSData *currentData, NSMutableDictionary *messageInternalIdToPreparedMessage);
+@property (nonatomic, copy) id (^dynamicDecorator)(int64_t, NSData *currentData, NSMutableDictionary *messageInternalIdToPreparedMessage);
 
-- (instancetype)initWithData:(NSData *)data metadata:(id)metadata shortMetadata:(id)shortMetadata;
-- (instancetype)initWithData:(NSData *)data metadata:(id)metadata shortMetadata:(id)shortMetadata messageId:(int64_t)messageId messageSeqNo:(int32_t)messageSeqNo;
+- (instancetype)initWithData:(NSData *)data metadata:(id)metadata additionalDebugDescription:(NSString *)additionalDebugDescription shortMetadata:(id)shortMetadata;
+- (instancetype)initWithData:(NSData *)data metadata:(id)metadata additionalDebugDescription:(NSString *)additionalDebugDescription shortMetadata:(id)shortMetadata messageId:(int64_t)messageId messageSeqNo:(int32_t)messageSeqNo;
 
 @end
