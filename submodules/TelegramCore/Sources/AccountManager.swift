6b728791c9f25733f2c1733c9de0017e05752bfa
Ilya Laktyushin <ilya@laktyush.in>
2020-02-12 20:33:23 +0400

    Fix

--- submodules/TelegramCore/Sources/AccountManager.swift
+++ submodules/TelegramCore/Sources/AccountManager.swift
@@ -153,6 +153,7 @@ private var declaredEncodables: Void = {
     declareEncodable(TelegramMediaWebpageAttribute.self, f: { TelegramMediaWebpageAttribute(decoder: $0) })
     declareEncodable(CachedPollOptionResult.self, f: { CachedPollOptionResult(decoder: $0) })
     //declareEncodable(ChatListFiltersState.self, f: { ChatListFiltersState(decoder: $0) })
+    declareEncodable(PeersNearbyState.self, f: { PeersNearbyState(decoder: $0) })
     
     return
 }()
