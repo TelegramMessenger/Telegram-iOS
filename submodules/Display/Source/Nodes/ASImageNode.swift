3ae2499491dc4f35e09f322320841a9acd987d04
Ali <>
2020-03-01 19:49:10 +0400

    Fix ASImageNode scale

--- submodules/Display/Source/Nodes/ASImageNode.swift
+++ submodules/Display/Source/Nodes/ASImageNode.swift
@@ -8,7 +8,8 @@ open class ASImageNode: ASDisplayNode {
             if let image = self.image {
                 let capInsets = image.capInsets
                 if capInsets.left.isZero && capInsets.top.isZero {
-                    self.contents = self.image?.cgImage
+                    self.contentsScale = image.scale
+                    self.contents = image.cgImage
                 } else {
                     ASDisplayNodeSetResizableContents(self, image)
                 }
