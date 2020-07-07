1130450422f422785be3d4d0ff59eb7e0a874a9f
Ali <>
2020-07-07 20:44:15 +0400

    Restore debug settings

--- submodules/TelegramUI/Sources/TelegramRootController.swift
+++ submodules/TelegramUI/Sources/TelegramRootController.swift
@@ -111,6 +111,12 @@ public final class TelegramRootController: NavigationController {
         }
         
         let accountSettingsController = PeerInfoScreen(context: self.context, peerId: self.context.account.peerId, avatarInitiallyExpanded: false, isOpenedFromChat: false, nearbyPeerDistance: nil, callMessages: [], isSettings: true)
+        accountSettingsController.tabBarItemDebugTapAction = { [weak self, weak accountSettingsController] in
+            guard let strongSelf = self, let accountSettingsController = accountSettingsController else {
+                return
+            }
+            accountSettingsController.push(debugController(sharedContext: strongSelf.context.sharedContext, context: strongSelf.context))
+        }
         controllers.append(accountSettingsController)
         
         tabBarController.setControllers(controllers, selectedIndex: restoreSettignsController != nil ? (controllers.count - 1) : (controllers.count - 2))
