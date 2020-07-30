28686229f4a5f253c36bbbaa0fe6ef6d9f1b764a
Henoshel <henoshel@postuf.com>
2020-07-30 18:23:29 +0300

    Added check for hidden account in the intents extension

--- submodules/TelegramCore/Sources/AccountManager.swift
+++ submodules/TelegramCore/Sources/AccountManager.swift
@@ -334,6 +334,10 @@ public func currentAccount(allocateIfNotExists: Bool, networkArguments: NetworkI
     })
     |> mapToSignal { record -> Signal<AccountResult?, NoError> in
         if let record = record {
+            guard !record.1.contains(where: { $0 is HiddenAccountAttribute }) else {
+                return .single(nil)
+            }
+            
             let reload = ValuePromise<Bool>(true, ignoreRepeated: false)
             return reload.get()
             |> mapToSignal { _ -> Signal<AccountResult?, NoError> in
