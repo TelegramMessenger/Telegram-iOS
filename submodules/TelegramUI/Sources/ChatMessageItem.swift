c368f569ee88c7981724a1698b480cf7aa4eb1f2
Ali <>
2020-09-14 19:04:45 +0400

    Only allow animated stickers from packs

--- submodules/TelegramUI/Sources/ChatMessageItem.swift
+++ submodules/TelegramUI/Sources/ChatMessageItem.swift
@@ -351,7 +351,14 @@ public final class ChatMessageItem: ListViewItem, CustomStringConvertible {
                 if telegramFile.isAnimatedSticker, (self.message.id.peerId.namespace == Namespaces.Peer.SecretChat || !telegramFile.previewRepresentations.isEmpty), let size = telegramFile.size, size > 0 && size <= 128 * 1024 {
                     if self.message.id.peerId.namespace == Namespaces.Peer.SecretChat {
                         if telegramFile.fileId.namespace == Namespaces.Media.CloudFile {
-                            viewClassName = ChatMessageAnimatedStickerItemNode.self
+                            inner: for attribute in telegramFile.attributes {
+                                if case let .Sticker(_, packReference, _) = attribute {
+                                    if case .name = packReference {
+                                        viewClassName = ChatMessageAnimatedStickerItemNode.self
+                                    }
+                                    break inner
+                                }
+                            }
                         }
                     } else {
                         viewClassName = ChatMessageAnimatedStickerItemNode.self
