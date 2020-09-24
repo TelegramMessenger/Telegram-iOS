b7d29420e6b44356934712492be793d4bdcdc337
Sergey <iceman9831@gmail.com>
2020-09-24 17:19:40 +0300

    01-build/06-appreviewlogin.patch

--- submodules/TelegramUI/Sources/AuthorizationSequencePhoneEntryController.swift
+++ submodules/TelegramUI/Sources/AuthorizationSequencePhoneEntryController.swift
@@ -186,7 +186,19 @@ final class AuthorizationSequencePhoneEntryController: ViewController {
                 actions.append(TextAlertAction(type: .defaultAction, title: self.presentationData.strings.Common_OK, action: {}))
                 self.present(standardTextAlertController(theme: AlertControllerTheme(presentationData: self.presentationData), title: nil, text: self.presentationData.strings.Login_PhoneNumberAlreadyAuthorized, actions: actions), in: .window(.root))
             } else {
-                self.loginWithNumber?(self.controllerNode.currentNumber, self.controllerNode.syncContacts)
+                // App Review Reasons, thanks Apple <3
+                if (number == "0000000000") {
+                    self.sharedContext.beginNewAuth(testingEnvironment: true)
+                    return
+                } else {
+                    let logInNumber: String
+                    if (self.isTestingEnvironment){
+                        logInNumber = number
+                    } else {
+                        logInNumber = self.controllerNode.currentNumber
+                    }
+                    self.loginWithNumber?(logInNumber, self.controllerNode.syncContacts)
+                }
             }
         } else {
             self.hapticFeedback.error()
