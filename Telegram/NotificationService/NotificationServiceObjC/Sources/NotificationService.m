a5fe5ff76d33a5fdee1e6d46e48c3ad873861516
Sergey <iceman9831@gmail.com>
2020-11-01 22:25:54 +0300

    [wip] hide account in notification

--- Telegram/NotificationService/NotificationServiceObjC/Sources/NotificationService.m
+++ Telegram/NotificationService/NotificationServiceObjC/Sources/NotificationService.m
@@ -362,6 +362,13 @@ static void reportMemory() {
             if ([soundString isKindOfClass:[NSString class]]) {
                 _bestAttemptContent.sound = [UNNotificationSound soundNamed:soundString];
             }
+            
+            NSString *appGroupName = [@"group." stringByAppendingString:_baseAppBundleId];
+            NSUserDefaults *sharedDefaults = [[NSUserDefaults alloc] initWithSuiteName:appGroupName];
+
+            BOOL *muteSoundSilent = [sharedDefaults boolForKey:@"muteSoundSilent"];
+            BOOL *hideNotifyAccountName = [sharedDefaults boolForKey:@"hideNotifyAccountName"];
+
             if (_isLockedValue) {
                 _bestAttemptContent.categoryIdentifier = @"locked";
             } else {
@@ -387,7 +394,9 @@ static void reportMemory() {
                 
                 if (accountInfos.accounts.count > 1) {
                     if (_bestAttemptContent.title.length != 0 && account.peerName.length != 0) {
-                        _bestAttemptContent.title = [NSString stringWithFormat:@"%@ → %@", _bestAttemptContent.title, account.peerName];
+                        if (!hideNotifyAccountName) {
+                            _bestAttemptContent.title = [NSString stringWithFormat:@"%@ → %@", _bestAttemptContent.title, account.peerName];
+                        }
                     }
                 }
             }
