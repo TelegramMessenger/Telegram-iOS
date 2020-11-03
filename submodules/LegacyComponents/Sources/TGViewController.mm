85295c0e07b336637b97b6aed3a4946bd13b703a
Ilya Laktyushin <ilya@laktyush.in>
2020-11-03 10:52:13 +0400

    Fix safe insets in legacy screens on iPhone 12

--- submodules/LegacyComponents/Sources/TGViewController.mm
+++ submodules/LegacyComponents/Sources/TGViewController.mm
@@ -1072,7 +1072,7 @@ static id<LegacyComponentsContext> _defaultContext = nil;
 + (UIEdgeInsets)safeAreaInsetForOrientation:(UIInterfaceOrientation)orientation hasOnScreenNavigation:(bool)hasOnScreenNavigation
 {
     int height = (int)TGScreenSize().height;
-    if (!TGIsPad() && (height != 812 && height != 896))
+    if (!TGIsPad() && (height != 812 && height != 896 && height != 780 && height != 844 && height != 926))
         return UIEdgeInsetsZero;
     
     if (TGIsPad()) {
