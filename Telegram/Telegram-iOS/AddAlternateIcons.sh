8ddb4088d88025e5677fd9e71209dd339058dd86
Ali <>
2020-11-05 21:43:11 +0400

    Fix tulsi build [skip ci]

--- Telegram/Telegram-iOS/AddAlternateIcons.sh
+++ Telegram/Telegram-iOS/AddAlternateIcons.sh
@@ -3,7 +3,11 @@ set -euo pipefail
 ADD_PLIST="${0}.runfiles/__main__/Telegram/Telegram-iOS/AlternateIcons.plist"
 ADD_PLIST_IPAD="${0}.runfiles/__main__/Telegram/Telegram-iOS/AlternateIcons-iPad.plist"
 
-INFO_PLIST="$1/Payload/Telegram.app/Info.plist"
+if [ -f "$1/Payload/Telegram.app/Info.plist" ]; then
+	INFO_PLIST="$1/Payload/Telegram.app/Info.plist"
+else
+	INFO_PLIST="$1/Telegram.app/Info.plist"
+fi
 
 /usr/libexec/PlistBuddy -c "add :CFBundleIcons:CFBundleAlternateIcons dict" "$INFO_PLIST"
 /usr/libexec/PlistBuddy -c "add :CFBundleIcons~ipad:CFBundleAlternateIcons dict" "$INFO_PLIST"
