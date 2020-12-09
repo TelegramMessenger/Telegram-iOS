907402750faa4fe16d893bdafbfd2d1d978dd733
Ali <>
2020-12-09 16:10:25 +0400

    Use appcenter-cli

--- buildbox/deploy-appcenter.sh
+++ buildbox/deploy-appcenter.sh
@@ -80,4 +80,5 @@ upload_dsym() {
     	"$API_HOST/v0.1/apps/$API_USER_NAME/$API_APP_NAME/symbol_uploads/$DSYM_UPLOAD_ID"
 }
 
-upload_ipa
+appcenter login --token "$API_TOKEN"
+appcenter distribute release --app "$API_USER_NAME/$API_APP_NAME" -f "$IPA_PATH" -g Internal
