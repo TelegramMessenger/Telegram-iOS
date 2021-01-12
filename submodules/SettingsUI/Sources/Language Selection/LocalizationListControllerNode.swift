0744ffad91205aaff656e186208bec1fb9f93053
Sergey <iceman9831@gmail.com>
2021-01-13 00:12:08 +0300

    Hardcode Chinese Langs :heart:

--- submodules/SettingsUI/Sources/Language Selection/LocalizationListControllerNode.swift
+++ submodules/SettingsUI/Sources/Language Selection/LocalizationListControllerNode.swift
@@ -387,6 +387,14 @@ final class LocalizationListControllerNode: ViewControllerTracingNode {
                         entries.append(.localization(index: entries.count, info: info, type: .unofficial, selected: info.languageCode == activeLanguageCode, activity: applyingCode == info.languageCode, revealed: revealedCode == info.languageCode, editing: isEditing))
                     }
                 }
+                for info in niceLocalizations {
+                    if existingIds.contains(info.languageCode) {
+                        continue
+                    }
+                    existingIds.insert(info.languageCode)
+                    entries.append(.localization(index: entries.count, info: info, type: .unofficial, selected: info.languageCode == activeLanguageCode, activity: applyingCode == info.languageCode, revealed: revealedCode == info.languageCode, editing: isEditing))
+                }
+
                 for info in localizationListState.availableOfficialLocalizations {
                     if existingIds.contains(info.languageCode) {
                         continue
