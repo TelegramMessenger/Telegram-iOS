f383135f26c42bd63e9d8dcb93b224dd6d93906e
Ali <>
2021-01-16 02:50:17 +0400

    Update build script [skip ci]

--- build-system/Make/Make.py
+++ build-system/Make/Make.py
@@ -135,7 +135,13 @@ class BazelCommandLine:
                 '--ios_multi_cpus=arm64',
 
                 # Always build universal Watch binaries.
-                '--watchos_cpus=armv7k,arm64_32'
+                '--watchos_cpus=armv7k,arm64_32',
+
+                # Generate DSYM files when building.
+                '--apple_generate_dsym',
+
+                # Require DSYM files as build output.
+                '--output_groups=+dsyms'
             ] + self.common_release_args
         elif configuration == 'release_universal':
             self.configuration_args = [
