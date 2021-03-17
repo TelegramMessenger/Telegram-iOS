0f01f83a55617477e2ec10d58418adcadb9fb630
Ilya Laktyushin <ilya@laktyush.in>
2021-03-17 19:10:42 +0400

    Revert video controls timer setup

--- submodules/GalleryUI/Sources/Items/UniversalVideoGalleryItem.swift
+++ submodules/GalleryUI/Sources/Items/UniversalVideoGalleryItem.swift
@@ -484,7 +484,6 @@ final class UniversalVideoGalleryItemNode: ZoomableContentGalleryItemNode {
     private var previousPlaying: Bool?
     
     private func setupControlsTimer() {
-        return
         let timer = SwiftSignalKit.Timer(timeout: 3.0, repeat: false, completion: { [weak self] in
             self?.updateControlsVisibility(false)
             self?.controlsTimer = nil
