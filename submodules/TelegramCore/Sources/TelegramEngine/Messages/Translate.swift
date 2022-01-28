3cd2e3763db5e1e1c943c2957de341ca732a0c9c
Ilya Laktyushin <ilya@laktyush.in>
2022-01-28 21:05:21 +0300

    Fix flags

--- submodules/TelegramCore/Sources/TelegramEngine/Messages/Translate.swift
+++ submodules/TelegramCore/Sources/TelegramEngine/Messages/Translate.swift
@@ -6,9 +6,9 @@ import MtProtoKit
 
 func _internal_translate(network: Network, text: String, fromLang: String?, toLang: String) -> Signal<String?, NoError> {
     var flags: Int32 = 0
-    flags |= 1
+    flags |= (1 << 1)
     if let _ = fromLang {
-        flags |= 2
+        flags |= (1 << 2)
     }
     
     return network.request(Api.functions.messages.translateText(flags: flags, peer: nil, msgId: nil, text: text, fromLang: fromLang, toLang: toLang))
