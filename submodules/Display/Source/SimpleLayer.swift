9750f70987e92be5ac6c3560dd3c02cc27037cbd
Ali <>
2022-02-18 20:13:02 +0400

    Cleanup

--- submodules/Display/Source/SimpleLayer.swift
+++ submodules/Display/Source/SimpleLayer.swift
@@ -1,13 +1,31 @@
 import UIKit
 
-private final class NullActionClass: NSObject, CAAction {
-    @objc func run(forKey event: String, object anObject: Any, arguments dict: [AnyHashable : Any]?) {
+public final class NullActionClass: NSObject, CAAction {
+    @objc public func run(forKey event: String, object anObject: Any, arguments dict: [AnyHashable : Any]?) {
     }
 }
 
-private let nullAction = NullActionClass()
+public let nullAction = NullActionClass()
 
 open class SimpleLayer: CALayer {
+    override open func action(forKey event: String) -> CAAction? {
+        return nullAction
+    }
+    
+    override public init() {
+        super.init()
+    }
+    
+    override public init(layer: Any) {
+        super.init(layer: layer)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+open class SimpleShapeLayer: CAShapeLayer {
     override open func action(forKey event: String) -> CAAction? {
         return nullAction
     }
