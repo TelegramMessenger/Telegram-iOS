4af274442433c36f65a9d24fb17a528794465933
Ilya Laktyushin <ilya@laktyush.in>
2022-04-26 04:52:16 +0400

    Improve rounded button shimmer

--- submodules/ShimmerEffect/Sources/ShimmerEffect.swift
+++ submodules/ShimmerEffect/Sources/ShimmerEffect.swift
@@ -1,6 +1,172 @@
 import UIKit
 import AsyncDisplayKit
 import Display
+import HierarchyTrackingLayer
+
+public final class ShimmerEffectForegroundView: UIView {
+    private var currentBackgroundColor: UIColor?
+    private var currentForegroundColor: UIColor?
+    private var currentHorizontal: Bool?
+    private var currentGradientSize: CGFloat?
+    private var currentDuration: Double?
+    private let imageContainer: SimpleLayer
+    private let image: SimpleLayer
+    
+    private var absoluteLocation: (CGRect, CGSize)?
+    private var isCurrentlyInHierarchy = false
+    private var shouldBeAnimating = false
+    
+    private let trackingLayer: HierarchyTrackingLayer
+    
+    public init() {
+        self.imageContainer = SimpleLayer()
+        
+        self.image = SimpleLayer()
+        self.image.contentsGravity = .resizeAspectFill
+        
+        self.trackingLayer = HierarchyTrackingLayer()
+        
+        super.init(frame: CGRect())
+        
+        self.clipsToBounds = true
+        self.isUserInteractionEnabled = false
+        
+        self.layer.addSublayer(self.imageContainer)
+        self.imageContainer.addSublayer(self.image)
+        
+        self.layer.addSublayer(self.trackingLayer)
+        
+        self.trackingLayer.didEnterHierarchy = { [weak self] in
+            guard let strongSelf = self else {
+                return
+            }
+            strongSelf.isCurrentlyInHierarchy = true
+            strongSelf.updateAnimation()
+        }
+        
+        self.trackingLayer.didExitHierarchy = { [weak self] in
+            guard let strongSelf = self else {
+                return
+            }
+            strongSelf.isCurrentlyInHierarchy = false
+            strongSelf.updateAnimation()
+        }
+    }
+    
+    required public init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func update(backgroundColor: UIColor, foregroundColor: UIColor, gradientSize: CGFloat?, duration: Double?, horizontal: Bool = false) {
+        if let currentBackgroundColor = self.currentBackgroundColor, currentBackgroundColor.isEqual(backgroundColor), let currentForegroundColor = self.currentForegroundColor, currentForegroundColor.isEqual(foregroundColor), self.currentHorizontal == horizontal, self.currentGradientSize == gradientSize {
+            return
+        }
+        self.currentBackgroundColor = backgroundColor
+        self.currentForegroundColor = foregroundColor
+        self.currentHorizontal = horizontal
+        self.currentGradientSize = gradientSize
+        self.currentDuration = duration
+        
+        let image: UIImage?
+        if horizontal {
+            image = generateImage(CGSize(width: gradientSize ?? 320.0, height: 16.0), opaque: false, scale: 1.0, rotatedContext: { size, context in
+                context.clear(CGRect(origin: CGPoint(), size: size))
+                context.setFillColor(backgroundColor.cgColor)
+                context.fill(CGRect(origin: CGPoint(), size: size))
+                
+                context.clip(to: CGRect(origin: CGPoint(), size: size))
+                
+                let transparentColor = foregroundColor.withAlphaComponent(0.0).cgColor
+                let peakColor = foregroundColor.cgColor
+                
+                var locations: [CGFloat] = [0.0, 0.5, 1.0]
+                let colors: [CGColor] = [transparentColor, peakColor, transparentColor]
+                
+                let colorSpace = CGColorSpaceCreateDeviceRGB()
+                let gradient = CGGradient(colorsSpace: colorSpace, colors: colors as CFArray, locations: &locations)!
+                
+                context.drawLinearGradient(gradient, start: CGPoint(x: 0.0, y: 0.0), end: CGPoint(x: size.width, y: 0.0), options: CGGradientDrawingOptions())
+            })
+        } else {
+            image = generateImage(CGSize(width: 16.0, height: gradientSize ?? 250.0), opaque: false, scale: 1.0, rotatedContext: { size, context in
+                context.clear(CGRect(origin: CGPoint(), size: size))
+                context.setFillColor(backgroundColor.cgColor)
+                context.fill(CGRect(origin: CGPoint(), size: size))
+                
+                context.clip(to: CGRect(origin: CGPoint(), size: size))
+                
+                let transparentColor = foregroundColor.withAlphaComponent(0.0).cgColor
+                let peakColor = foregroundColor.cgColor
+                
+                var locations: [CGFloat] = [0.0, 0.5, 1.0]
+                let colors: [CGColor] = [transparentColor, peakColor, transparentColor]
+                
+                let colorSpace = CGColorSpaceCreateDeviceRGB()
+                let gradient = CGGradient(colorsSpace: colorSpace, colors: colors as CFArray, locations: &locations)!
+                
+                context.drawLinearGradient(gradient, start: CGPoint(x: 0.0, y: 0.0), end: CGPoint(x: 0.0, y: size.height), options: CGGradientDrawingOptions())
+            })
+        }
+        self.image.contents = image?.cgImage
+        self.updateAnimation()
+    }
+    
+    public func updateAbsoluteRect(_ rect: CGRect, within containerSize: CGSize) {
+        if let absoluteLocation = self.absoluteLocation, absoluteLocation.0 == rect && absoluteLocation.1 == containerSize {
+            return
+        }
+        let sizeUpdated = self.absoluteLocation?.1 != containerSize
+        let frameUpdated = self.absoluteLocation?.0 != rect
+        self.absoluteLocation = (rect, containerSize)
+        
+        if sizeUpdated {
+            if self.shouldBeAnimating {
+                self.image.removeAnimation(forKey: "shimmer")
+                self.addImageAnimation()
+            } else {
+                self.updateAnimation()
+            }
+        }
+        
+        if frameUpdated {
+            self.imageContainer.frame = CGRect(origin: CGPoint(x: -rect.minX, y: -rect.minY), size: containerSize)
+        }
+    }
+    
+    private func updateAnimation() {
+        let shouldBeAnimating = self.isCurrentlyInHierarchy && self.absoluteLocation != nil && self.currentHorizontal != nil
+        if shouldBeAnimating != self.shouldBeAnimating {
+            self.shouldBeAnimating = shouldBeAnimating
+            if shouldBeAnimating {
+                self.addImageAnimation()
+            } else {
+                self.image.removeAnimation(forKey: "shimmer")
+            }
+        }
+    }
+    
+    private func addImageAnimation() {
+        guard let containerSize = self.absoluteLocation?.1, let horizontal = self.currentHorizontal else {
+            return
+        }
+        
+        if horizontal {
+            let gradientSize = self.currentGradientSize ?? 320.0
+            self.image.frame = CGRect(origin: CGPoint(x: -gradientSize, y: 0.0), size: CGSize(width: gradientSize, height: containerSize.height))
+            let animation = self.image.makeAnimation(from: 0.0 as NSNumber, to: (containerSize.width + gradientSize) as NSNumber, keyPath: "position.x", timingFunction: CAMediaTimingFunctionName.easeOut.rawValue, duration: self.currentDuration ?? 1.3, delay: 0.0, mediaTimingFunction: nil, removeOnCompletion: true, additive: true)
+            animation.repeatCount = Float.infinity
+            animation.beginTime = 1.0
+            self.image.add(animation, forKey: "shimmer")
+        } else {
+            let gradientSize = self.currentGradientSize ?? 250.0
+            self.image.frame = CGRect(origin: CGPoint(x: 0.0, y: -gradientSize), size: CGSize(width: containerSize.width, height: gradientSize))
+            let animation = self.image.makeAnimation(from: 0.0 as NSNumber, to: (containerSize.height + gradientSize) as NSNumber, keyPath: "position.y", timingFunction: CAMediaTimingFunctionName.easeOut.rawValue, duration: self.currentDuration ?? 1.3, delay: 0.0, mediaTimingFunction: nil, removeOnCompletion: true, additive: true)
+            animation.repeatCount = Float.infinity
+            animation.beginTime = 1.0
+            self.image.add(animation, forKey: "shimmer")
+        }
+    }
+}
 
 final class ShimmerEffectForegroundNode: ASDisplayNode {
     private var currentBackgroundColor: UIColor?
