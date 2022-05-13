fff3b03105fe2d79562af80f8d24fb2540823a1a
Ilya Laktyushin <ilya@laktyush.in>
2022-05-13 06:02:56 +0400

    Update CI

--- buildbox/guest-build-telegram.sh
+++ buildbox/guest-build-telegram.sh
@@ -81,7 +81,7 @@ security import "build-system/AppleWWDRCAG3.cer" -k "$MY_KEYCHAIN" -P "" -T /usr
 
 security set-key-partition-list -S apple-tool:,apple: -k "$MY_KEYCHAIN_PASSWORD" "$MY_KEYCHAIN"
 
-if [ "$1" == "hockeyapp" ] || [ "$1" == "appcenter-experimental" ] || [ "$1" == "appcenter-experimental-2" ]; then
+if [ "$1" == "hockeyapp" ] || [ "$1" == "appcenter-experimental" ] || [ "$1" == "appcenter-experimental-2" ] || [ "$1" == "App Store-development" ]; then
 	APP_CONFIGURATION="release_arm64"
 elif [ "$1" == "appstore" ]; then
 	APP_CONFIGURATION="release_universal"
