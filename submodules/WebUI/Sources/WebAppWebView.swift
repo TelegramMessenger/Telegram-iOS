ace926f2c8941093d5f82f3a842c0987b46c7993
Ilya Laktyushin <ilya@laktyush.in>
2022-05-26 15:02:20 +0400

    Fix web app contenteditable focus

--- submodules/WebUI/Sources/WebAppWebView.swift
+++ submodules/WebUI/Sources/WebAppWebView.swift
@@ -55,7 +55,7 @@ final class WebAppWebView: WKWebView {
             handleScriptMessageImpl?(message)
         }, name: "performAction")
         
-        let selectionString = "var css = '*{-webkit-touch-callout:none;} :not(input):not(textarea){-webkit-user-select:none;}';"
+        let selectionString = "var css = '*{-webkit-touch-callout:none;} :not(input):not(textarea):not([\"contenteditable\"=\"true\"]){-webkit-user-select:none;}';"
                 + " var head = document.head || document.getElementsByTagName('head')[0];"
                 + " var style = document.createElement('style'); style.type = 'text/css';" +
                 " style.appendChild(document.createTextNode(css)); head.appendChild(style);"
