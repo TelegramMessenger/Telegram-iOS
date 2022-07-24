ccdd8efc6ec29f945a6fa372b2f52cae20f0fcdd
Ali <>
2022-07-24 23:27:31 +0200

    Emoji improvements

--- submodules/TelegramUI/Components/EntityKeyboard/Sources/EntityKeyboardTopPanelComponent.swift
+++ submodules/TelegramUI/Components/EntityKeyboard/Sources/EntityKeyboardTopPanelComponent.swift
@@ -515,6 +515,7 @@ final class EntityKeyboardStaticStickersPanelComponent: Component {
             self.scrollView.alwaysBounceHorizontal = false
             self.scrollView.scrollsToTop = false
             self.scrollView.delegate = self
+            self.scrollView.clipsToBounds = false
             
             self.scrollViewContainer.addSubview(self.scrollView)
             self.addSubview(self.scrollViewContainer)
@@ -682,7 +683,7 @@ final class EntityKeyboardStaticStickersPanelComponent: Component {
             transition.setCornerRadius(layer: self.scrollViewContainer.layer, cornerRadius: min(availableSize.width / 2.0, availableSize.height / 2.0))
             
             self.ignoreScrolling = true
-            self.scrollView.frame = CGRect(origin: CGPoint(), size: CGSize(width: max(availableSize.width, 126.0), height: availableSize.height))
+            self.scrollView.frame = CGRect(origin: CGPoint(), size: CGSize(width: max(availableSize.width, 0.0), height: availableSize.height))
             self.scrollView.contentSize = itemLayout.contentSize
             self.ignoreScrolling = false
             
@@ -1902,7 +1903,7 @@ final class EntityKeyboardTopPanelComponent: Component {
                         if itemLayout.isExpanded {
                             expandedInset = -2.0
                         } else {
-                            expandedInset = -itemLayout.sideInset - (itemFrame.width + itemLayout.itemSpacing) * 2.0
+                            expandedInset = -itemLayout.sideInset - (itemLayout.itemSize.width + itemLayout.itemSpacing) * 2.0
                         }
                         self.scrollView.scrollRectToVisible(itemFrame.insetBy(dx: expandedInset, dy: 0.0), animated: true)
                         break
