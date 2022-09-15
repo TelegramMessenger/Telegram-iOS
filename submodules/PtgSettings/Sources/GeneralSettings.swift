e4e8eb6fbf702210cf4d45c86c102c4b3ac39602
G <G@G.G>
2022-09-15 20:25:44 +0300

    Added option to disable 'live text'.

--- submodules/PtgSettings/Sources/GeneralSettings.swift
+++ submodules/PtgSettings/Sources/GeneralSettings.swift
@@ -7,19 +7,21 @@ public struct PtgSettings: Codable, Equatable {
     public let showPeerId: Bool
     public let suppressForeignAgentNotice: Bool
     public let enableForeignAgentNoticeSearchFiltering: Bool // makes sense only if suppressForeignAgentNotice is true
+    public let enableLiveText: Bool
     
     public static var defaultSettings: PtgSettings {
-        return PtgSettings(showPeerId: true, suppressForeignAgentNotice: true, enableForeignAgentNoticeSearchFiltering: true)
+        return PtgSettings(showPeerId: true, suppressForeignAgentNotice: true, enableForeignAgentNoticeSearchFiltering: true, enableLiveText: false)
     }
     
     public var effectiveEnableForeignAgentNoticeSearchFiltering: Bool {
         return self.suppressForeignAgentNotice && self.enableForeignAgentNoticeSearchFiltering
     }
     
-    public init(showPeerId: Bool, suppressForeignAgentNotice: Bool, enableForeignAgentNoticeSearchFiltering: Bool) {
+    public init(showPeerId: Bool, suppressForeignAgentNotice: Bool, enableForeignAgentNoticeSearchFiltering: Bool, enableLiveText: Bool) {
         self.showPeerId = showPeerId
         self.suppressForeignAgentNotice = suppressForeignAgentNotice
         self.enableForeignAgentNoticeSearchFiltering = enableForeignAgentNoticeSearchFiltering
+        self.enableLiveText = enableLiveText
     }
     
     public init(from decoder: Decoder) throws {
@@ -28,6 +30,7 @@ public struct PtgSettings: Codable, Equatable {
         self.showPeerId = (try container.decodeIfPresent(Int32.self, forKey: "spi") ?? 1) != 0
         self.suppressForeignAgentNotice = (try container.decodeIfPresent(Int32.self, forKey: "sfan") ?? 1) != 0
         self.enableForeignAgentNoticeSearchFiltering = (try container.decodeIfPresent(Int32.self, forKey: "efansf") ?? 1) != 0
+        self.enableLiveText = (try container.decodeIfPresent(Int32.self, forKey: "elt") ?? 0) != 0
     }
     
     public func encode(to encoder: Encoder) throws {
@@ -36,6 +39,7 @@ public struct PtgSettings: Codable, Equatable {
         try container.encode((self.showPeerId ? 1 : 0) as Int32, forKey: "spi")
         try container.encode((self.suppressForeignAgentNotice ? 1 : 0) as Int32, forKey: "sfan")
         try container.encode((self.enableForeignAgentNoticeSearchFiltering ? 1 : 0) as Int32, forKey: "efansf")
+        try container.encode((self.enableLiveText ? 1 : 0) as Int32, forKey: "elt")
     }
     
     public init(_ entry: PreferencesEntry?) {
@@ -48,14 +52,18 @@ public struct PtgSettings: Codable, Equatable {
     }
     
     public func withUpdated(showPeerId: Bool) -> PtgSettings {
-        return PtgSettings(showPeerId: showPeerId, suppressForeignAgentNotice: self.suppressForeignAgentNotice, enableForeignAgentNoticeSearchFiltering: self.enableForeignAgentNoticeSearchFiltering)
+        return PtgSettings(showPeerId: showPeerId, suppressForeignAgentNotice: self.suppressForeignAgentNotice, enableForeignAgentNoticeSearchFiltering: self.enableForeignAgentNoticeSearchFiltering, enableLiveText: self.enableLiveText)
     }
     
     public func withUpdated(suppressForeignAgentNotice: Bool) -> PtgSettings {
-        return PtgSettings(showPeerId: self.showPeerId, suppressForeignAgentNotice: suppressForeignAgentNotice, enableForeignAgentNoticeSearchFiltering: self.enableForeignAgentNoticeSearchFiltering)
+        return PtgSettings(showPeerId: self.showPeerId, suppressForeignAgentNotice: suppressForeignAgentNotice, enableForeignAgentNoticeSearchFiltering: self.enableForeignAgentNoticeSearchFiltering, enableLiveText: self.enableLiveText)
     }
     
     public func withUpdated(enableForeignAgentNoticeSearchFiltering: Bool) -> PtgSettings {
-        return PtgSettings(showPeerId: self.showPeerId, suppressForeignAgentNotice: self.suppressForeignAgentNotice, enableForeignAgentNoticeSearchFiltering: enableForeignAgentNoticeSearchFiltering)
+        return PtgSettings(showPeerId: self.showPeerId, suppressForeignAgentNotice: self.suppressForeignAgentNotice, enableForeignAgentNoticeSearchFiltering: enableForeignAgentNoticeSearchFiltering, enableLiveText: self.enableLiveText)
+    }
+    
+    public func withUpdated(enableLiveText: Bool) -> PtgSettings {
+        return PtgSettings(showPeerId: self.showPeerId, suppressForeignAgentNotice: self.suppressForeignAgentNotice, enableForeignAgentNoticeSearchFiltering: self.enableForeignAgentNoticeSearchFiltering, enableLiveText: enableLiveText)
     }
 }
