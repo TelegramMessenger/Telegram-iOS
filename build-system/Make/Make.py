c8f7a38140fc1f0ec40e4f439f562fe5252d90b7
Ali <>
2022-09-17 01:55:51 +0400

    Drop 32-bit watch and upgrade to bazel 5.3.0

--- build-system/Make/Make.py
+++ build-system/Make/Make.py
@@ -144,7 +144,7 @@ class BazelCommandLine:
                 '--ios_multi_cpus=armv7,arm64',
 
                 # Always build universal Watch binaries.
-                '--watchos_cpus=armv7k,arm64_32'
+                '--watchos_cpus=arm64_32'
             ] + self.common_debug_args
         elif configuration == 'debug_arm64':
             self.configuration_args = [
@@ -155,7 +155,7 @@ class BazelCommandLine:
                 '--ios_multi_cpus=arm64',
 
                 # Always build universal Watch binaries.
-                '--watchos_cpus=armv7k,arm64_32'
+                '--watchos_cpus=arm64_32'
             ] + self.common_debug_args
         elif configuration == 'debug_sim_arm64':
             self.configuration_args = [
@@ -166,7 +166,7 @@ class BazelCommandLine:
                 '--ios_multi_cpus=sim_arm64',
 
                 # Always build universal Watch binaries.
-                '--watchos_cpus=armv7k,arm64_32'
+                '--watchos_cpus=arm64_32'
             ] + self.common_debug_args
         elif configuration == 'debug_armv7':
             self.configuration_args = [
@@ -176,7 +176,7 @@ class BazelCommandLine:
                 '--ios_multi_cpus=armv7',
 
                 # Always build universal Watch binaries.
-                '--watchos_cpus=armv7k,arm64_32'
+                '--watchos_cpus=arm64_32'
             ] + self.common_debug_args
         elif configuration == 'release_arm64':
             self.configuration_args = [
@@ -187,7 +187,7 @@ class BazelCommandLine:
                 '--ios_multi_cpus=arm64',
 
                 # Always build universal Watch binaries.
-                '--watchos_cpus=armv7k,arm64_32',
+                '--watchos_cpus=arm64_32',
 
                 # Generate DSYM files when building.
                 '--apple_generate_dsym',
@@ -204,7 +204,7 @@ class BazelCommandLine:
                 '--ios_multi_cpus=armv7',
 
                 # Always build universal Watch binaries.
-                '--watchos_cpus=armv7k,arm64_32',
+                '--watchos_cpus=arm64_32',
 
                 # Generate DSYM files when building.
                 '--apple_generate_dsym',
@@ -221,7 +221,7 @@ class BazelCommandLine:
                 '--ios_multi_cpus=armv7,arm64',
 
                 # Always build universal Watch binaries.
-                '--watchos_cpus=armv7k,arm64_32',
+                '--watchos_cpus=arm64_32',
                 
                 # Generate DSYM files when building.
                 '--apple_generate_dsym',
@@ -411,7 +411,7 @@ def resolve_codesigning(arguments, base_path, build_configuration, provisioning_
             bundle_id=build_configuration.bundle_id,
             codesigning_type=arguments.gitCodesigningType,
             password=password,
-            always_fetch=arguments.gitCodesigningAlwaysFetch
+            always_fetch=not arguments.gitCodesigningUseCurrent
         )
     elif arguments.codesigningInformationPath is not None:
         profile_source = DirectoryCodesigningSource(
@@ -666,10 +666,10 @@ def add_codesigning_common_arguments(current_parser: argparse.ArgumentParser):
     )
 
     current_parser.add_argument(
-        '--gitCodesigningAlwaysFetch',
+        '--gitCodesigningUseCurrent',
         action='store_true',
         required=False,
-        default=True,
+        default=False,
         help='''
             Always refresh codesigning repository.
             '''
