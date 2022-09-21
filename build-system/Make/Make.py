09dd6149a1b05d32ceaf4bf126d8540f858d6ffe
Ali <>
2022-09-22 01:25:08 +0200

    Add strip

--- build-system/Make/Make.py
+++ build-system/Make/Make.py
@@ -190,7 +190,7 @@ class BazelCommandLine:
                 '--ios_multi_cpus=arm64',
 
                 # Always build universal Watch binaries.
-                '--watchos_cpus=armv7k,arm64_32',
+                '--watchos_cpus=arm64_32',
 
                 # Generate DSYM files when building.
                 '--apple_generate_dsym',
