a5c36af0f4bcbf399fe33b5a56a57df1a50f7081
Mike Renoir <>
2023-01-09 14:58:03 +0400

    macos typo

--- submodules/Postbox/Sources/StorageBox/StorageBox.swift
+++ submodules/Postbox/Sources/StorageBox/StorageBox.swift
@@ -669,7 +669,7 @@ public final class StorageBox {
             let queue = self.queue
             return ActionDisposable { [weak self] in
                 queue.async {
-                    guard let self else {
+                    guard let `self` = self else {
                         return
                     }
                     self.totalSizeSubscribers.remove(index)
