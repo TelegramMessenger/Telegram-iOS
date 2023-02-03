524e8f1e8134ecec15b2dec44d3c0eada4c47ae7
Ali <>
2023-02-03 20:44:43 +0100

    Fix build

--- submodules/TelegramCore/Sources/State/PendingMessageManager.swift
+++ submodules/TelegramCore/Sources/State/PendingMessageManager.swift
@@ -1076,7 +1076,7 @@ public final class PendingMessageManager {
                 
                 let sendMessageRequest: Signal<NetworkRequestResult<Api.Updates>, MTRpcError>
                 switch content.content {
-                    case let .text:
+                    case .text:
                         if bubbleUpEmojiOrStickersets {
                             flags |= Int32(1 << 15)
                         }
