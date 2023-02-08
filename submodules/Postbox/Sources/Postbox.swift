ffa4be373a1ee0a0d84e1a0d6194ed47a458892b
Ali <>
2023-02-08 10:37:01 +0400

    Remove debugging

--- submodules/Postbox/Sources/Postbox.swift
+++ submodules/Postbox/Sources/Postbox.swift
@@ -1302,7 +1302,7 @@ public func openPostbox(basePath: String, seedConfiguration: SeedConfiguration, 
 
             #if DEBUG
             //debugSaveState(basePath: basePath + "/db", name: "previous2")
-            debugRestoreState(basePath: basePath + "/db", name: "previous2")
+            //debugRestoreState(basePath: basePath + "/db", name: "previous2")
             #endif
             
             let startTime = CFAbsoluteTimeGetCurrent()
