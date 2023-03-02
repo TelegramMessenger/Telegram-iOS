98c0b7234c459cbac653edd28c5c3254c636267a
Ali <>
2023-03-02 17:00:51 +0400

    UI improvements

--- submodules/TelegramUI/Sources/PeerInfo/PeerInfoScreen.swift
+++ submodules/TelegramUI/Sources/PeerInfo/PeerInfoScreen.swift
@@ -10771,7 +10771,7 @@ func presentAddMembersImpl(context: AccountContext, updatedPresentationData: (in
                             })
                         }
                     } else {
-                        if let exportedInvitation, let link = exportedInvitation.link {
+                        if "".isEmpty {
                             let _ = (context.engine.data.get(
                                 EngineDataList(failedPeerIds.compactMap { item -> EnginePeer.Id? in
                                     return item.0
@@ -10782,7 +10782,7 @@ func presentAddMembersImpl(context: AccountContext, updatedPresentationData: (in
                                 if !peers.isEmpty, let contactsController, let navigationController = contactsController.navigationController as? NavigationController {
                                     var viewControllers = navigationController.viewControllers
                                     if let index = viewControllers.firstIndex(where: { $0 === contactsController }) {
-                                        let inviteScreen = SendInviteLinkScreen(context: context, link: link, peers: peers)
+                                        let inviteScreen = SendInviteLinkScreen(context: context, link: exportedInvitation?.link, peers: peers)
                                         viewControllers.remove(at: index)
                                         viewControllers.append(inviteScreen)
                                         navigationController.setViewControllers(viewControllers, animated: true)
