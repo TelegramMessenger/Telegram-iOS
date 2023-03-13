ef49e3de9cad0a2279d64c74ea12e1ee54771568
Ali <>
2023-03-13 16:06:07 +0400

    Fix blurred components appearance

--- submodules/TelegramUI/Sources/AppDelegate.swift
+++ submodules/TelegramUI/Sources/AppDelegate.swift
@@ -514,6 +514,7 @@ private func extractAccountManagerState(records: AccountRecordsView<TelegramAcco
         TempBox.initializeShared(basePath: rootPath, processType: "app", launchSpecificId: Int64.random(in: Int64.min ... Int64.max))
         
         let writeAbilityTestFile = TempBox.shared.tempFile(fileName: "test.bin")
+        var writeAbilityTestSuccess = true
         if let testFile = ManagedFile(queue: nil, path: writeAbilityTestFile.path, mode: .readwrite) {
             let bufferSize = 128 * 1024
             let randomBuffer = malloc(bufferSize)!
@@ -522,29 +523,28 @@ private func extractAccountManagerState(records: AccountRecordsView<TelegramAcco
             }
             arc4random_buf(randomBuffer, bufferSize)
             var writtenBytes = 0
-            var success = true
             while writtenBytes < 1024 * 1024 {
                 let actualBytes = testFile.write(randomBuffer, count: bufferSize)
                 writtenBytes += actualBytes
                 if actualBytes != bufferSize {
-                    success = false
+                    writeAbilityTestSuccess = false
                     break
                 }
             }
             testFile._unsafeClose()
             TempBox.shared.dispose(writeAbilityTestFile)
-            
-            if !success {
-                let alertController = UIAlertController(title: nil, message: "There is not enough free space on the device.", preferredStyle: .alert)
-                alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-                    preconditionFailure()
-                }))
-                self.mainWindow?.presentNative(alertController)
-                
-                return true
-            }
         } else {
-            preconditionFailure()
+            writeAbilityTestSuccess = false
+        }
+        
+        if !writeAbilityTestSuccess {
+            let alertController = UIAlertController(title: nil, message: "The device does not have sufficient free space.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                preconditionFailure()
+            }))
+            self.mainWindow?.presentNative(alertController)
+            
+            return true
         }
         
         let legacyLogs: [String] = [
