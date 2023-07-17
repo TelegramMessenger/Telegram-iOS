42be53c03ab86224064a226cb2c5088b882a2c9e
Ilya Laktyushin <ilya@laktyush.in>
2023-07-17 21:09:41 +0200

    Story privacy badge

--- submodules/TelegramUI/Components/Stories/StoryContainerScreen/Sources/StoryPrivacyIconComponent.swift
+++ submodules/TelegramUI/Components/Stories/StoryContainerScreen/Sources/StoryPrivacyIconComponent.swift
@@ -41,48 +41,50 @@ final class StoryPrivacyIconComponent: Component {
             self.state = state
             
             let size = CGSize(width: component.isEditable ? 40.0 : 24.0, height: 24.0)
-            self.image = generateImage(size, rotatedContext: { size, context in
+            self.image = generateImage(size, contextGenerator: { size, context in
+                let bounds = CGRect(origin: .zero, size: size)
+                context.clear(bounds)
+                
                 let path: CGPath
                 if size.width == size.height {
-                    path = CGPath(ellipseIn: CGRect(origin: .zero, size: size), transform: nil)
+                    path = CGPath(ellipseIn: bounds, transform: nil)
                 } else {
-                    path = CGPath(roundedRect: CGRect(origin: .zero, size: size), cornerWidth: size.height / 2.0, cornerHeight: size.height / 2.0, transform: nil)
+                    path = CGPath(roundedRect: bounds, cornerWidth: size.height / 2.0, cornerHeight: size.height / 2.0, transform: nil)
                 }
                 
                 context.addPath(path)
                 context.clip()
                 
-                var locations: [CGFloat] = [0.0, 1.0]
+                var locations: [CGFloat] = [1.0, 0.0]
                 let colors: [CGColor]
-                let icon: UIImage
+                var icon: UIImage?
                 
                 switch component.privacy {
                 case .everyone:
                     colors = [UIColor(rgb: 0x4faaff).cgColor, UIColor(rgb: 0x017aff).cgColor]
-                    icon = UIImage()
+                    icon = UIImage(bundleImageName: "Stories/PrivacyEveryone")
                 case .closeFriends:
                     colors = [UIColor(rgb: 0x87d93a).cgColor, UIColor(rgb: 0x31b73b).cgColor]
-                    icon = UIImage()
+                    icon = UIImage(bundleImageName: "Stories/PrivacyCloseFriends")
                 case .contacts:
                     colors = [UIColor(rgb: 0xc36eff).cgColor, UIColor(rgb: 0x8c61fa).cgColor]
-                    icon = UIImage()
+                    icon = UIImage(bundleImageName: "Stories/PrivacyContacts")
                 case .selectedContacts:
                     colors = [UIColor(rgb: 0xffb643).cgColor, UIColor(rgb: 0xf69a36).cgColor]
-                    icon = UIImage()
+                    icon = UIImage(bundleImageName: "Stories/PrivacySelectedContacts")
                 }
                 
                 let colorSpace = CGColorSpaceCreateDeviceRGB()
                 let gradient = CGGradient(colorsSpace: colorSpace, colors: colors as CFArray, locations: &locations)!
                 context.drawLinearGradient(gradient, start: CGPoint(x: 0.0, y: 0.0), end: CGPoint(x: 0.0, y: size.height), options: CGGradientDrawingOptions())
                 
-                if let cgImage = icon.cgImage {
-                    context.draw(cgImage, in: CGRect(origin: .zero, size: icon.size))
+                if let icon, let cgImage = icon.cgImage {
+                    context.draw(cgImage, in: CGRect(origin: CGPoint(x: component.isEditable ? 1.0 : 0.0, y: 0.0), size: icon.size))
                 }
                 
                 if component.isEditable {
-                    let arrowIcon = UIImage()
-                    if let cgImage = arrowIcon.cgImage {
-                        context.draw(cgImage, in: CGRect(origin: .zero, size: icon.size))
+                    if let arrowIcon = UIImage(bundleImageName: "Stories/PrivacyDownArrow"), let cgImage = arrowIcon.cgImage {
+                        context.draw(cgImage, in: CGRect(origin: CGPoint(x: size.width - arrowIcon.size.width - 6.0, y: floorToScreenPixels((size.height - arrowIcon.size.height) / 2.0)), size: arrowIcon.size))
                     }
                 }
             })
