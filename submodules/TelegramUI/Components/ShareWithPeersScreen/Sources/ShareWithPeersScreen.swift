4dad31f6eb60a411e1922611956a4189aaf1c7a0
Ilya Laktyushin <ilya@laktyush.in>
2023-07-18 16:47:15 +0200

    Fix colors

--- submodules/TelegramUI/Components/ShareWithPeersScreen/Sources/ShareWithPeersScreen.swift
+++ submodules/TelegramUI/Components/ShareWithPeersScreen/Sources/ShareWithPeersScreen.swift
@@ -2023,7 +2023,7 @@ public class ShareWithPeersScreen: ViewControllerComponentContainer {
                 id: .contacts,
                 title: presentationData.strings.Story_Privacy_CategoryContacts,
                 icon: "Chat List/Tabs/IconContacts",
-                iconColor: .yellow,
+                iconColor: .violet,
                 actionTitle: contactsSubtitle
             ))
             
@@ -2063,7 +2063,7 @@ public class ShareWithPeersScreen: ViewControllerComponentContainer {
                 id: .selectedContacts,
                 title: presentationData.strings.Story_Privacy_CategorySelectedContacts,
                 icon: "Chat List/Filters/Group",
-                iconColor: .violet,
+                iconColor: .yellow,
                 actionTitle: selectedContactsSubtitle
             ))
             
