0051d8977ee12638aba75e957a24c1757cf66b43
Ali <>
2023-10-27 16:23:32 +0400

    Fix build

--- submodules/TranslateUI/Sources/ChatTranslation.swift
+++ submodules/TranslateUI/Sources/ChatTranslation.swift
@@ -207,7 +207,14 @@ public func chatTranslationState(context: AccountContext, peerId: EnginePeer.Id)
                             for message in messages {
                                 if message.effectivelyIncoming(context.account.peerId), message.text.count >= 10 {
                                     var text = String(message.text.prefix(256))
-                                    if var entities = message.textEntitiesAttribute?.entities.filter({ [.Pre, .Code, .Url, .Email, .Mention, .Hashtag, .BotCommand].contains($0.type) }) {
+                                    if var entities = message.textEntitiesAttribute?.entities.filter({ entity in
+                                        switch entity.type {
+                                        case .Pre, .Code, .Url, .Email, .Mention, .Hashtag, .BotCommand:
+                                            return true
+                                        default:
+                                            return false
+                                        }
+                                    }) {
                                         entities = entities.sorted(by: { $0.range.lowerBound > $1.range.lowerBound })
                                         var ranges: [Range<String.Index>] = []
                                         for entity in entities {
