40fd8d7566d7236c6fe3beb41b01e53ef7bfca0a
Ali <>
2023-11-08 17:00:57 +0400

    Revert "Fix NetworkHelper deallocation after adopting weak in MTContext"
    This reverts commit 2ba969a5cb7f0bb249160f451a72a4ca1a3d09c0.

--- submodules/TelegramCore/Sources/Network/Network.swift
+++ submodules/TelegramCore/Sources/Network/Network.swift
@@ -662,9 +662,6 @@ private final class NetworkHelper: NSObject, MTContextChangeListener {
         self.contextLoggedOutUpdated = contextLoggedOutUpdated
     }
     
-    deinit {
-    }
-    
     func fetchContextDatacenterPublicKeys(_ context: MTContext, datacenterId: Int) -> MTSignal {
         return MTSignal { subscriber in
             let disposable = self.requestPublicKeys(datacenterId).start(next: { next in
@@ -740,7 +737,6 @@ public final class Network: NSObject, MTRequestMessageServiceDelegate {
     private let queue: Queue
     public let datacenterId: Int
     public let context: MTContext
-    private var networkHelper: NetworkHelper?
     let mtProto: MTProto
     let requestService: MTRequestMessageService
     let basePath: String
@@ -815,7 +811,7 @@ public final class Network: NSObject, MTRequestMessageServiceDelegate {
         }
         
         let _contextProxyId = self._contextProxyId
-        let networkHelper = NetworkHelper(requestPublicKeys: { [weak self] id in
+        context.add(NetworkHelper(requestPublicKeys: { [weak self] id in
             if let strongSelf = self {
                 return strongSelf.request(Api.functions.help.getCdnConfig())
                 |> map(Optional.init)
@@ -856,9 +852,7 @@ public final class Network: NSObject, MTRequestMessageServiceDelegate {
         }, contextLoggedOutUpdated: { [weak self] in
             Logger.shared.log("Network", "contextLoggedOut")
             self?.loggedOut?()
-        })
-        self.networkHelper = networkHelper
-        context.add(networkHelper)
+        }))
         requestService.delegate = self
         
         self._multiplexedRequestManager = MultiplexedRequestManager(takeWorker: { [weak self] target, tag, continueInBackground in
