41f05c73d8016efd2e343abd96d5ddc1016efa44
Isaac <>
2023-12-18 02:07:17 +0400

    Cache resolved app bundle

--- submodules/AppBundle/Sources/AppBundle/AppBundle.m
+++ submodules/AppBundle/Sources/AppBundle/AppBundle.m
@@ -1,15 +1,21 @@
 #import <AppBundle/AppBundle.h>
 
 NSBundle * _Nonnull getAppBundle() {
-    NSBundle *bundle = [NSBundle mainBundle];
-    if ([[bundle.bundleURL pathExtension] isEqualToString:@"appex"]) {
-        bundle = [NSBundle bundleWithURL:[[bundle.bundleURL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent]];
-    } else if ([[bundle.bundleURL pathExtension] isEqualToString:@"framework"]) {
-        bundle = [NSBundle bundleWithURL:[[bundle.bundleURL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent]];
-    } else if ([[bundle.bundleURL pathExtension] isEqualToString:@"Frameworks"]) {
-        bundle = [NSBundle bundleWithURL:[bundle.bundleURL URLByDeletingLastPathComponent]];
-    }
-    return bundle;
+    static NSBundle *appBundle = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSBundle *bundle = [NSBundle mainBundle];
+        if ([[bundle.bundleURL pathExtension] isEqualToString:@"appex"]) {
+            bundle = [NSBundle bundleWithURL:[[bundle.bundleURL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent]];
+        } else if ([[bundle.bundleURL pathExtension] isEqualToString:@"framework"]) {
+            bundle = [NSBundle bundleWithURL:[[bundle.bundleURL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent]];
+        } else if ([[bundle.bundleURL pathExtension] isEqualToString:@"Frameworks"]) {
+            bundle = [NSBundle bundleWithURL:[bundle.bundleURL URLByDeletingLastPathComponent]];
+        }
+        appBundle = bundle;
+    });
+    
+    return appBundle;
 }
 
 @implementation UIImage (AppBundle)
