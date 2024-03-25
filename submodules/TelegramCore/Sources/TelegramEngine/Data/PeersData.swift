101098bf817555f1973d1eb8e56373c840c956df
Isaac <>
2024-03-25 19:35:00 +0400

    Fix build

--- submodules/TelegramCore/Sources/TelegramEngine/Data/PeersData.swift
+++ submodules/TelegramCore/Sources/TelegramEngine/Data/PeersData.swift
@@ -852,34 +852,6 @@ public extension TelegramEngine.EngineData.Item {
             }
         }
         
-        public struct PersonalChannel: TelegramEngineDataItem, TelegramEngineMapKeyDataItem, PostboxViewDataItem {
-            public typealias Result = CachedTelegramPersonalChannel
-
-            fileprivate var id: EnginePeer.Id
-            public var mapKey: EnginePeer.Id {
-                return self.id
-            }
-
-            public init(id: EnginePeer.Id) {
-                self.id = id
-            }
-
-            var key: PostboxViewKey {
-                return .cachedPeerData(peerId: self.id)
-            }
-
-            func extract(view: PostboxView) -> Result {
-                guard let view = view as? CachedPeerDataView else {
-                    preconditionFailure()
-                }
-                if let cachedData = view.cachedPeerData as? CachedUserData {
-                    return cachedData.personalChannel
-                } else {
-                    return .unknown
-                }
-            }
-        }
-        
         public struct PersonalPhoto: TelegramEngineDataItem, TelegramEngineMapKeyDataItem, PostboxViewDataItem {
             public typealias Result = EnginePeerCachedInfoItem<TelegramMediaImage?>
 
