0a9f514e035c7f634ff99c3840aafdd1c33b8c1c
Mikhail Filimonov <overtakeful@gmail.com>
2024-11-14 13:19:53 +0400

    - error

--- submodules/TelegramCore/Sources/TelegramEngine/Payments/BotPaymentForm.swift
+++ submodules/TelegramCore/Sources/TelegramEngine/Payments/BotPaymentForm.swift
@@ -172,6 +172,7 @@ extension BotPaymentMethod {
 
 public enum BotPaymentFormRequestError {
     case generic
+    case alreadyActive
 }
 
 extension BotPaymentInvoice {
@@ -385,8 +386,12 @@ func _internal_fetchBotPaymentInvoice(postbox: Postbox, network: Network, source
         let flags: Int32 = 0
 
         return network.request(Api.functions.payments.getPaymentForm(flags: flags, invoice: invoice, themeParams: nil))
-        |> `catch` { _ -> Signal<Api.payments.PaymentForm, BotPaymentFormRequestError> in
-            return .fail(.generic)
+        |> `catch` { error -> Signal<Api.payments.PaymentForm, BotPaymentFormRequestError> in
+            if error.errorDescription == "SUBSCRIPTION_ALREADY_ACTIVE" {
+                return .fail(.alreadyActive)
+            } else {
+                return .fail(.generic)
+            }
         }
         |> mapToSignal { result -> Signal<TelegramMediaInvoice, BotPaymentFormRequestError> in
             return postbox.transaction { transaction -> TelegramMediaInvoice in
