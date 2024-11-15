c8912caee630310667dadc61238619ebc683d785
Ilya Laktyushin <ilya@laktyush.in>
2024-11-15 19:38:05 +0400

    Various fixes

--- submodules/TelegramUI/Components/Gifts/GiftViewScreen/Sources/GiftViewScreen.swift
+++ submodules/TelegramUI/Components/Gifts/GiftViewScreen/Sources/GiftViewScreen.swift
@@ -1110,7 +1110,7 @@ public class GiftViewScreen: ViewControllerComponentContainer {
             |> filter { !$0.isEmpty }
             |> deliverOnMainQueue).start(next: { giftOptions in
                 let premiumOptions = giftOptions.filter { $0.users == 1 }.map { CachedPremiumGiftOption(months: $0.months, currency: $0.currency, amount: $0.amount, botUrl: "", storeProductId: $0.storeProductId) }
-                let controller = context.sharedContext.makeGiftOptionsController(context: context, peerId: peerId, premiumOptions: premiumOptions)
+                let controller = context.sharedContext.makeGiftOptionsController(context: context, peerId: peerId, premiumOptions: premiumOptions, hasBirthday: false)
                 self.push(controller)
             })
         }
