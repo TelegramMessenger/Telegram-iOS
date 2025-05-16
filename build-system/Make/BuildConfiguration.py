b394d7a958767f48567fc7611be12745b0627f6b
Isaac <>
2025-05-17 02:34:15 +0800

    Cleanup

--- build-system/Make/BuildConfiguration.py
+++ build-system/Make/BuildConfiguration.py
@@ -106,7 +106,13 @@ def decrypt_codesigning_directory_recursively(source_base_path, destination_base
         destination_path = destination_base_path + '/' + file_name
         allowed_file_extensions = ['.mobileprovision', '.cer', '.p12']
         if os.path.isfile(source_path) and any(source_path.endswith(ext) for ext in allowed_file_extensions):
-            decrypt_match_data(source_path, destination_path, password)
+            #print('Decrypting {} to {} with {}'.format(source_path, destination_path, password))
+            os.system('ruby build-system/decrypt.rb "{password}" "{source_path}" "{destination_path}"'.format(
+                password=password,
+                source_path=source_path,
+                destination_path=destination_path
+            ))
+            #decrypt_match_data(source_path, destination_path, password)
         elif os.path.isdir(source_path):
             os.makedirs(destination_path, exist_ok=True)
             decrypt_codesigning_directory_recursively(source_path, destination_path, password)
