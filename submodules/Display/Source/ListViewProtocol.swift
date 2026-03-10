18201ca8300f0b0ecfe60958ca8380d02c068b58
Isaac <>
2026-03-10 08:54:43 +0100

    Refactor

--- submodules/Display/Source/ListViewProtocol.swift
+++ submodules/Display/Source/ListViewProtocol.swift
@@ -13,24 +13,15 @@ public protocol ListView: ASDisplayNode {
     var keepMinimalScrollHeightWithTopInset: CGFloat? { get set }
     var itemNodeHitTest: ((CGPoint) -> Bool)? { get set }
     var stackFromBottom: Bool { get set }
-    var stackFromBottomInsetItemFactor: CGFloat { get set }
     var limitHitTestToNodes: Bool { get set }
     var keepTopItemOverscrollBackground: ListViewKeepTopItemOverscrollBackground? { get set }
-    var keepBottomItemOverscrollBackground: UIColor? { get set }
-    var snapToBottomInsetUntilFirstInteraction: Bool { get set }
     var allowInsetFixWhileTracking: Bool { get set }
     var visualInsets: UIEdgeInsets? { get set }
     var dynamicVisualInsets: (() -> UIEdgeInsets)? { get set }
     var verticalScrollIndicatorColor: UIColor? { get set }
     var verticalScrollIndicatorFollowsOverscroll: Bool { get set }
-    var globalIgnoreScrollingEvents: Bool { get set }
-    var ignoreStopScrolling: Bool { get set }
-    var synchronousNodes: Bool { get set }
-    var debugInfo: Bool { get set }
-    var useSingleDimensionTouchPoint: Bool { get set }
     var defaultToSynchronousTransactionWhileScrolling: Bool { get set }
     var enableExtractedBackgrounds: Bool { get set }
-    var itemHeaderNodesAlpha: CGFloat { get set }
     var autoScrollWhenReordering: Bool { get set }
     var reorderedItemHasShadow: Bool { get set }
     var reorderingRequiresLongPress: Bool { get set }
@@ -38,19 +29,13 @@ public protocol ListView: ASDisplayNode {
     var accessibilityPageScrolledString: ((String, String) -> String)? { get set }
 
     // MARK: - Read-only State Properties
-    var visibleSize: CGSize { get }
     var insets: UIEdgeInsets { get }
-    var headerInsets: UIEdgeInsets { get }
-    var scrollIndicatorInsets: UIEdgeInsets { get }
     var isTracking: Bool { get }
     var trackingOffset: CGFloat { get }
     var beganTrackingAtTopOrigin: Bool { get }
     var isDragging: Bool { get }
-    var isDeceleratingAfterTracking: Bool { get }
-    var isReordering: Bool { get }
     var edgeEffectExtension: CGFloat { get }
     var displayedItemRange: ListViewDisplayedItemRange { get }
-    var internalDisplayedItemRange: ListViewDisplayedItemRange? { get }
     var opaqueTransactionState: Any? { get }
 
     // MARK: - Callbacks
@@ -61,17 +46,13 @@ public protocol ListView: ASDisplayNode {
     var endedInteractiveDragging: (CGPoint) -> Void { get set }
     var didEndScrolling: ((Bool) -> Void)? { get set }
     var didEndScrollingWithOverscroll: (() -> Void)? { get set }
-    var generalScrollDirectionUpdated: (GeneralScrollDirection) -> Void { get set }
     var updateFloatingHeaderOffset: ((CGFloat, ContainedViewLayoutTransition) -> Void)? { get set }
     var didScrollWithOffset: ((CGFloat, ContainedViewLayoutTransition, ListViewItemNode?, Bool) -> Void)? { get set }
     var addContentOffset: ((CGFloat, ListViewItemNode?) -> Void)? { get set }
     var shouldStopScrolling: ((CGFloat) -> Bool)? { get set }
     var onContentsUpdated: ((ContainedViewLayoutTransition) -> Void)? { get set }
     var onEdgeEffectExtensionUpdated: ((ContainedViewLayoutTransition) -> Void)? { get set }
-    var updateScrollingIndicator: ((ListViewImpl.ScrollingIndicatorState?, ContainedViewLayoutTransition) -> Void)? { get set }
     var tapped: (() -> Void)? { get set }
-    var willBeginReorder: (CGPoint) -> Void { get set }
-    var reorderBegan: () -> Void { get set }
     var reorderItem: (Int, Int, Any?) -> Signal<Bool, NoError> { get set }
     var reorderCompleted: (Any?) -> Void { get set }
 
@@ -93,35 +74,19 @@ public protocol ListView: ASDisplayNode {
     func visibleContentOffset() -> ListViewVisibleContentOffset
     func visibleBottomContentOffset() -> ListViewVisibleContentOffset
     func stopScrolling()
-    func cancelTracking()
     @discardableResult func scrollToOffsetFromTop(_ offset: CGFloat, animated: Bool) -> Bool
-    @discardableResult func scrollWithDirection(_ direction: ListViewScrollDirection, distance: CGFloat) -> Bool
     func transferVelocity(_ velocity: CGFloat)
     func resetScrolledToItem()
 
-    func itemIndexAtPoint(_ point: CGPoint) -> Int?
-    func itemNodeAtIndex(_ index: Int) -> ListViewItemNode?
-    func indexOf(itemNode: ListViewItemNode) -> Int?
     func forEachItemNode(_ f: (ASDisplayNode) -> Void)
-    func forEachRemovedItemNode(_ f: (ASDisplayNode) -> Void)
     func forEachVisibleItemNode(_ f: (ASDisplayNode) -> Void)
     func enumerateItemNodes(_ f: (ASDisplayNode) -> Bool)
     func forEachItemHeaderNode(_ f: (ListViewItemHeaderNode) -> Void)
     func forEachAccessoryItemNode(_ f: (ListViewAccessoryItemNode) -> Void)
 
     func ensureItemNodeVisible(_ node: ListViewItemNode, animated: Bool, overflow: CGFloat, allowIntersection: Bool, atTop: Bool, curve: ListViewAnimationCurve)
-    func ensureItemNodeVisibleAtTopInset(_ node: ListViewItemNode)
-    func itemNodeRelativeOffset(_ node: ListViewItemNode) -> CGFloat?
-    func itemNodeVisibleInsideInsets(_ node: ListViewItemNode) -> Bool
 
     func clearHighlightAnimated(_ animated: Bool)
-    func updateNodeHighlightsAnimated(_ animated: Bool)
-    func cancelSelection()
-    func updateVisibleItemRange(force: Bool)
-    func flashHeaderItems(duration: Double)
-    func withTransaction(_ f: @escaping () -> Void)
-
-    func customItemDeleteAnimationDuration(itemNode: ListViewItemNode) -> Double?
 }
 
 // MARK: - Default Parameter Values
@@ -151,14 +116,6 @@ public extension ListView {
             updateOpaqueState: updateOpaqueState,
             completion: completion
         )
-    }
-
-    func updateVisibleItemRange() {
-        self.updateVisibleItemRange(force: false)
-    }
-
-    func flashHeaderItems() {
-        self.flashHeaderItems(duration: 2.0)
     }
 
     func ensureItemNodeVisible(_ node: ListViewItemNode, animated: Bool = true, overflow: CGFloat = 0.0, allowIntersection: Bool = false, atTop: Bool = false, curve: ListViewAnimationCurve = .Default(duration: 0.25)) {
